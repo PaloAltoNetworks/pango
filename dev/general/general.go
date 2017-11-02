45a7df75009a917b44dae1b32ca8957565f6f6e2
Garfield Freeman <gfreeman@paloaltonetworks.com>
2017-11-02 11:36:32 -0700

    Adding rawxml graft points for the rest of the device's general config; adding Domain and UpdateServer params

--- dev/general/general.go
+++ dev/general/general.go
@@ -31,6 +31,8 @@ type Config struct {
     Netmask string
     Gateway string
     Timezone string
+    Domain string
+    UpdateServer string
     DnsPrimary string
     DnsSecondary string
     NtpPrimaryAddress string
@@ -43,6 +45,18 @@ type Config struct {
     NtpSecondaryKeyId int
     NtpSecondaryAlgorithm string
     NtpSecondaryAuthKey string
+
+    raw map[string] string
+}
+
+// Defaults sets params with uninitialized values to their GUI default setting.
+//
+// The defaults are as follows:
+//      * UpdateServer: updates.paloaltonetworks.com
+func (o *Config) Defaults() {
+    if o.UpdateServer == "" {
+        o.UpdateServer = "updates.paloaltonetworks.com"
+    }
 }
 
 // Merge copies non connectivity variables from source Config `s` to this
@@ -58,6 +72,14 @@ func (o *Config) Merge(s Config) {
 
     if s.Timezone != "" {
         o.Timezone = s.Timezone
+    }
+
+    if s.Domain != "" {
+        o.Domain = s.Domain
+    }
+
+    if s.UpdateServer != "" {
+        o.UpdateServer = s.UpdateServer
     }
 
     if s.DnsPrimary != "" {
@@ -109,7 +131,7 @@ func (o *Config) Merge(s Config) {
     }
 }
 
-// Gset is a namespace struct, included as part of pango.Client.
+// General is a namespace struct, included as part of pango.Client.
 type General struct {
     con util.XapiClient
 }
@@ -162,7 +184,7 @@ func (c *General) versioning() (normalizer, func(Config) (interface{})) {
     return &container_v1{}, specify_v1
 }
 
-func (c *General) details(fn func(interface{}, interface{}, interface{}) ([]byte, error)) (Config, error) {
+func (c *General) details(fn util.Retriever) (Config, error) {
     path := c.xpath()
     obj, _ := c.versioning()
     if _, err := fn(path, nil, obj); err != nil {
@@ -200,6 +222,8 @@ func (o *container_v1) Normalize() Config {
         Netmask: o.Answer.Netmask,
         Gateway: o.Answer.Gateway,
         Timezone: o.Answer.Timezone,
+        Domain: o.Answer.Domain,
+        UpdateServer: o.Answer.UpdateServer,
     }
     if o.Answer.Dns != nil {
         ans.DnsPrimary = o.Answer.Dns.Primary
@@ -248,6 +272,110 @@ func (o *container_v1) Normalize() Config {
         }
     }
 
+    ans.raw = make(map[string] string)
+    if o.Answer.AckLoginBanner != nil {
+        ans.raw["alb"] = util.CleanRawXml(o.Answer.AckLoginBanner.Text)
+    }
+    if o.Answer.AuthenticationProfile != nil {
+        ans.raw["ap"] = util.CleanRawXml(o.Answer.AuthenticationProfile.Text)
+    }
+    if o.Answer.CertificateProfile != nil {
+        ans.raw["cp"] = util.CleanRawXml(o.Answer.CertificateProfile.Text)
+    }
+    if o.Answer.DomainLookupUrl != nil {
+        ans.raw["dlu"] = util.CleanRawXml(o.Answer.DomainLookupUrl.Text)
+    }
+    if o.Answer.FqdnForceRefreshTime != nil {
+        ans.raw["ffrt"] = util.CleanRawXml(o.Answer.FqdnForceRefreshTime.Text)
+    }
+    if o.Answer.FqdnRefreshTime != nil {
+        ans.raw["frt"] = util.CleanRawXml(o.Answer.FqdnRefreshTime.Text)
+    }
+    if o.Answer.GeoLocation != nil {
+        ans.raw["gl"] = util.CleanRawXml(o.Answer.GeoLocation.Text)
+    }
+    if o.Answer.HsmSettings != nil {
+        ans.raw["hs"] = util.CleanRawXml(o.Answer.HsmSettings.Text)
+    }
+    if o.Answer.IpAddressLookupUrl != nil {
+        ans.raw["ialu"] = util.CleanRawXml(o.Answer.IpAddressLookupUrl.Text)
+    }
+    if o.Answer.Ipv6Address != nil {
+        ans.raw["i6a"] = util.CleanRawXml(o.Answer.Ipv6Address.Text)
+    }
+    if o.Answer.Ipv6DefaultGateway != nil {
+        ans.raw["i6dg"] = util.CleanRawXml(o.Answer.Ipv6DefaultGateway.Text)
+    }
+    if o.Answer.Locale != nil {
+        ans.raw["locale"] = util.CleanRawXml(o.Answer.Locale.Text)
+    }
+    if o.Answer.LogExportSchedule != nil {
+        ans.raw["les"] = util.CleanRawXml(o.Answer.LogExportSchedule.Text)
+    }
+    if o.Answer.LogLink != nil {
+        ans.raw["ll"] = util.CleanRawXml(o.Answer.LogLink.Text)
+    }
+    if o.Answer.LoginBanner != nil {
+        ans.raw["lb"] = util.CleanRawXml(o.Answer.LoginBanner.Text)
+    }
+    if o.Answer.MotdAndBanner != nil {
+        ans.raw["mab"] = util.CleanRawXml(o.Answer.MotdAndBanner.Text)
+    }
+    if o.Answer.Mtu != nil {
+        ans.raw["mtu"] = util.CleanRawXml(o.Answer.Mtu.Text)
+    }
+    if o.Answer.PanoramaServer != nil {
+        ans.raw["ps"] = util.CleanRawXml(o.Answer.PanoramaServer.Text)
+    }
+    if o.Answer.PanoramaServer2 != nil {
+        ans.raw["ps2"] = util.CleanRawXml(o.Answer.PanoramaServer2.Text)
+    }
+    if o.Answer.PermittedIp != nil {
+        ans.raw["pi"] = util.CleanRawXml(o.Answer.PermittedIp.Text)
+    }
+    if o.Answer.Route != nil {
+        ans.raw["route"] = util.CleanRawXml(o.Answer.Route.Text)
+    }
+    if o.Answer.SecureProxyPassword != nil {
+        ans.raw["sppassword"] = util.CleanRawXml(o.Answer.SecureProxyPassword.Text)
+    }
+    if o.Answer.SecureProxyPort != nil {
+        ans.raw["spport"] = util.CleanRawXml(o.Answer.SecureProxyPort.Text)
+    }
+    if o.Answer.SecureProxyServer != nil {
+        ans.raw["sps"] = util.CleanRawXml(o.Answer.SecureProxyServer.Text)
+    }
+    if o.Answer.SecureProxyUser != nil {
+        ans.raw["spu"] = util.CleanRawXml(o.Answer.SecureProxyUser.Text)
+    }
+    if o.Answer.ServerVerification != nil {
+        ans.raw["sv"] = util.CleanRawXml(o.Answer.ServerVerification.Text)
+    }
+    if o.Answer.Service != nil {
+        ans.raw["service"] = util.CleanRawXml(o.Answer.Service.Text)
+    }
+    if o.Answer.SnmpSetting != nil {
+        ans.raw["ss"] = util.CleanRawXml(o.Answer.SnmpSetting.Text)
+    }
+    if o.Answer.SpeedDuplex != nil {
+        ans.raw["sd"] = util.CleanRawXml(o.Answer.SpeedDuplex.Text)
+    }
+    if o.Answer.SslTlsServiceProfile != nil {
+        ans.raw["stsp"] = util.CleanRawXml(o.Answer.SslTlsServiceProfile.Text)
+    }
+    if o.Answer.SyslogCertificate != nil {
+        ans.raw["sc"] = util.CleanRawXml(o.Answer.SyslogCertificate.Text)
+    }
+    if o.Answer.Type != nil {
+        ans.raw["type"] = util.CleanRawXml(o.Answer.Type.Text)
+    }
+    if o.Answer.UpdateSchedule != nil {
+        ans.raw["us"] = util.CleanRawXml(o.Answer.UpdateSchedule.Text)
+    }
+    if len(ans.raw) == 0 {
+        ans.raw = nil
+    }
+
     return ans
 }
 
@@ -258,8 +386,43 @@ type config_v1 struct {
     Netmask string `xml:"netmask"`
     Gateway string `xml:"default-gateway"`
     Timezone string `xml:"timezone"`
+    Domain string `xml:"domain,omitempty"`
+    UpdateServer string `xml:"update-server,omitempty"`
     Dns *deviceDns `xml:"dns-setting"`
     Ntp *deviceNtp `xml:"ntp-servers"`
+    AckLoginBanner *util.RawXml `xml:"ack-login-banner"`
+    AuthenticationProfile *util.RawXml `xml:"authentication-profile"`
+    CertificateProfile *util.RawXml `xml:"certificate-profile"`
+    DomainLookupUrl *util.RawXml `xml:"domain-lookup-url"`
+    FqdnForceRefreshTime *util.RawXml `xml:"fqdn-forcerefresh-time"`
+    FqdnRefreshTime *util.RawXml `xml:"fqdn-refresh-time"`
+    GeoLocation *util.RawXml `xml:"geo-location"`
+    HsmSettings *util.RawXml `xml:"hsm-settings"`
+    IpAddressLookupUrl *util.RawXml `xml:"ip-address-lookup-url"`
+    Ipv6Address *util.RawXml `xml:"ipv6-address"`
+    Ipv6DefaultGateway *util.RawXml `xml:"ipv6-default-gateway"`
+    Locale *util.RawXml `xml:"locale"`
+    LogExportSchedule *util.RawXml `xml:"log-export-schedule"`
+    LogLink *util.RawXml `xml:"log-link"`
+    LoginBanner *util.RawXml `xml:"login-banner"`
+    MotdAndBanner *util.RawXml `xml:"motd-and-banner"`
+    Mtu *util.RawXml `xml:"mtu"`
+    PanoramaServer *util.RawXml `xml:"panorama-server"`
+    PanoramaServer2 *util.RawXml `xml:"panorama-server-2"`
+    PermittedIp *util.RawXml `xml:"permitted-ip"`
+    Route *util.RawXml `xml:"route"`
+    SecureProxyPassword *util.RawXml `xml:"secure-proxy-password"`
+    SecureProxyPort *util.RawXml `xml:"secure-proxy-port"`
+    SecureProxyServer *util.RawXml `xml:"secure-proxy-server"`
+    SecureProxyUser *util.RawXml `xml:"secure-proxy-user"`
+    ServerVerification *util.RawXml `xml:"server-verification"`
+    Service *util.RawXml `xml:"service"`
+    SnmpSetting *util.RawXml `xml:"snmp-setting"`
+    SpeedDuplex *util.RawXml `xml:"speed-duplex"`
+    SslTlsServiceProfile *util.RawXml `xml:"ssl-tls-service-profile"`
+    SyslogCertificate *util.RawXml `xml:"syslog-certificate"`
+    Type *util.RawXml `xml:"type"`
+    UpdateSchedule *util.RawXml `xml:"update-schedule"`
 }
 
 type deviceDns struct {
@@ -304,6 +467,8 @@ func specify_v1(c Config) interface{} {
         Netmask: c.Netmask,
         Gateway: c.Gateway,
         Timezone: c.Timezone,
+        Domain: c.Domain,
+        UpdateServer: c.UpdateServer,
     }
     if c.DnsPrimary != "" || c.DnsSecondary != "" {
         ans.Dns = &deviceDns{
@@ -359,6 +524,106 @@ func specify_v1(c Config) interface{} {
     }
     if ntp_config.Primary != nil || ntp_config.Secondary != nil {
         ans.Ntp = ntp_config
+    }
+
+    if text, present := c.raw["alb"]; present {
+        ans.AckLoginBanner = &util.RawXml{text}
+    }
+    if text, present := c.raw["ap"]; present {
+        ans.AuthenticationProfile = &util.RawXml{text}
+    }
+    if text, present := c.raw["cp"]; present {
+        ans.CertificateProfile = &util.RawXml{text}
+    }
+    if text, present := c.raw["dlu"]; present {
+        ans.DomainLookupUrl = &util.RawXml{text}
+    }
+    if text, present := c.raw["ffrt"]; present {
+        ans.FqdnForceRefreshTime = &util.RawXml{text}
+    }
+    if text, present := c.raw["frt"]; present {
+        ans.FqdnRefreshTime = &util.RawXml{text}
+    }
+    if text, present := c.raw["gl"]; present {
+        ans.GeoLocation = &util.RawXml{text}
+    }
+    if text, present := c.raw["hs"]; present {
+        ans.HsmSettings = &util.RawXml{text}
+    }
+    if text, present := c.raw["ialu"]; present {
+        ans.IpAddressLookupUrl = &util.RawXml{text}
+    }
+    if text, present := c.raw["i6a"]; present {
+        ans.Ipv6Address = &util.RawXml{text}
+    }
+    if text, present := c.raw["i6dg"]; present {
+        ans.Ipv6DefaultGateway = &util.RawXml{text}
+    }
+    if text, present := c.raw["locale"]; present {
+        ans.Locale = &util.RawXml{text}
+    }
+    if text, present := c.raw["les"]; present {
+        ans.LogExportSchedule = &util.RawXml{text}
+    }
+    if text, present := c.raw["ll"]; present {
+        ans.LogLink = &util.RawXml{text}
+    }
+    if text, present := c.raw["lb"]; present {
+        ans.LoginBanner = &util.RawXml{text}
+    }
+    if text, present := c.raw["mab"]; present {
+        ans.MotdAndBanner = &util.RawXml{text}
+    }
+    if text, present := c.raw["mtu"]; present {
+        ans.Mtu = &util.RawXml{text}
+    }
+    if text, present := c.raw["ps"]; present {
+        ans.PanoramaServer = &util.RawXml{text}
+    }
+    if text, present := c.raw["ps2"]; present {
+        ans.PanoramaServer2 = &util.RawXml{text}
+    }
+    if text, present := c.raw["pi"]; present {
+        ans.PermittedIp = &util.RawXml{text}
+    }
+    if text, present := c.raw["route"]; present {
+        ans.Route = &util.RawXml{text}
+    }
+    if text, present := c.raw["sppassword"]; present {
+        ans.SecureProxyPassword = &util.RawXml{text}
+    }
+    if text, present := c.raw["spport"]; present {
+        ans.SecureProxyPort = &util.RawXml{text}
+    }
+    if text, present := c.raw["sps"]; present {
+        ans.SecureProxyServer = &util.RawXml{text}
+    }
+    if text, present := c.raw["spu"]; present {
+        ans.SecureProxyUser = &util.RawXml{text}
+    }
+    if text, present := c.raw["sv"]; present {
+        ans.ServerVerification = &util.RawXml{text}
+    }
+    if text, present := c.raw["service"]; present {
+        ans.Service = &util.RawXml{text}
+    }
+    if text, present := c.raw["ss"]; present {
+        ans.SnmpSetting = &util.RawXml{text}
+    }
+    if text, present := c.raw["sd"]; present {
+        ans.SpeedDuplex = &util.RawXml{text}
+    }
+    if text, present := c.raw["stsp"]; present {
+        ans.SslTlsServiceProfile = &util.RawXml{text}
+    }
+    if text, present := c.raw["sc"]; present {
+        ans.SyslogCertificate = &util.RawXml{text}
+    }
+    if text, present := c.raw["type"]; present {
+        ans.Type = &util.RawXml{text}
+    }
+    if text, present := c.raw["us"]; present {
+        ans.UpdateSchedule = &util.RawXml{text}
     }
 
     return ans
