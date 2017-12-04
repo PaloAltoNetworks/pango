249816f4011234d97eca3eda78709b69d3e8547c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2017-12-04 14:45:10 -0800

    Fixing zone XML creation - omitempty for log setting and profile

--- netw/zone/zone.go
+++ netw/zone/zone.go
@@ -232,8 +232,8 @@ type entry_v1 struct {
     VWire *zoneInterfaceList `xml:"network>virtual-wire"`
     Tap *zoneInterfaceList `xml:"network>tap"`
     External *zoneInterfaceList `xml:"network>external"`
-    Profile string `xml:"network>zone-protection-profile"`
-    LogSetting string `xml:"network>log-setting"`
+    Profile string `xml:"network>zone-protection-profile,omitempty"`
+    LogSetting string `xml:"network>log-setting,omitempty"`
     EnableUserId string `xml:"enable-user-identification"`
     IncludeAcl *aclList `xml:"user-acl>include-list"`
     ExcludeAcl *aclList `xml:"user-acl>exclude-list"`
