647244a8950a28498e7f155a567defbfad2b6c00
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-01-02 22:30:21 -0800

    adding new function to testdata and client interface

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -41,6 +41,7 @@ func (c *MockClient) LogQuery(f string, a...interface{}) {}
 func (c *MockClient) LogOp(f string, a...interface{}) {}
 func (c *MockClient) LogUid(f string, a...interface{}) {}
 func (c *MockClient) Commit(d string, e, f, g, h bool) (uint, error) { return 0, nil }
+func (c *MockClient) PositionFirstEntity(d int, e, f string, g, h []string) error { return nil }
 
 func (c *MockClient) Op(req interface{}, vsys string, extras interface{}, ans interface{}) ([]byte, error) {
     c.Function = "op"
