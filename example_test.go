0af9352bf7e931904a2076ba0e98272a4ac9c09c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-05-15 15:35:30 -0700

    updating docs; adding godoc badge

---
+++ example_test.go
@@ -0,0 +1,48 @@
+package pango_test
+
+import (
+    "log"
+
+    "github.com/PaloAltoNetworks/pango"
+    "github.com/PaloAltoNetworks/pango/netw/interface/eth"
+)
+
+func Example() {
+    var err error
+
+    // Connect to the firewall.
+    fw := pango.Firewall{Client: pango.Client{
+        Hostname: "192.168.1.1",
+        Username: "admin",
+        Password: "admin",
+    }}
+    if err = fw.Initialize(); err != nil {
+        log.Fatalf("Failed to connect to %s: %s", fw.Hostname, err)
+    }
+
+    // Define the ethernet interface we want to configure.
+    e := eth.Entry{
+        Name: "ethernet1/7",
+        Mode: "layer3",
+        Comment: "Made by pango",
+        StaticIps: []string{"10.1.1.1/24", "10.2.1.1/24"},
+    }
+
+    // If the interface is already present, leave it alone.
+    ethList, err := fw.Network.EthernetInterface.GetList()
+    if err != nil {
+        log.Fatalf("Failed to get interface listing: %s", err)
+    }
+    for i := range ethList {
+        if ethList[i] == e.Name {
+            log.Printf("Interface %q already exists, quitting.", e.Name)
+            return
+        }
+    }
+
+    // Interface is not there, configure it.
+    if err = fw.Network.EthernetInterface.Set("vsys1", e); err != nil {
+        log.Fatalf("Failed to create %q: %s", e.Name, err)
+    }
+    log.Printf("Created %q ok", e.Name)
+}
