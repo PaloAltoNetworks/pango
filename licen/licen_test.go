2eb3308edef460f6f64bccddcd79a8704dd0e566
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-05-23 17:09:43 -0700

    fix PAN-OS authcode activation returning non-xml

--- licen/licen_test.go
+++ licen/licen_test.go
@@ -1,6 +1,7 @@
 package licen
 
 import (
+    "fmt"
     "testing"
 
     "github.com/PaloAltoNetworks/pango/testdata"
@@ -20,6 +21,43 @@ func TestCurrent(t *testing.T) {
     }
     if len(ans) != 2 {
         t.Fail()
+    }
+}
+
+func TestAuthCode(t *testing.T) {
+    testCases := []struct{
+        desc string
+        shouldFail bool
+        resp testdata.Response
+    }{
+        {"fail xml response", true, testdata.Response{
+            []byte("<response status=\"error\"><msg><line>Failed to install licenses. Invalid Auth Code: foo</line></msg></response>"),
+            fmt.Errorf("Failed to install licenses. Invalid Auth Code: foo"),
+        }},
+        {"success non xml response", false, testdata.Response{
+            []byte("VM Device License installed. Restarting pan services."),
+            fmt.Errorf("EOF"),
+        }},
+        {"other non xml response", true, testdata.Response{
+            []byte("foobar"),
+            fmt.Errorf("EOF"),
+        }},
+    }
+
+    ns := &Licen{}
+
+    for _, tc := range testCases {
+        t.Run(tc.desc, func(t *testing.T) {
+            mc := &testdata.MockClient{Resp: []testdata.Response{tc.resp}}
+            ns.Initialize(mc)
+
+            err := ns.Activate("foo")
+            if tc.shouldFail && err == nil {
+                t.Errorf("Expected failure, but got nil error back")
+            } else if !tc.shouldFail && err != nil {
+                t.Errorf("Expected success, but got error: %s", err)
+            }
+        })
     }
 }
 
