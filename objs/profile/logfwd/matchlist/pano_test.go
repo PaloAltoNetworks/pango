e7d1f12d52450d67abc7bc32cb1ba0684a9ef7c1
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-06-25 10:20:47 -0700

    add support for logfwd'ing match lists

---
+++ objs/profile/logfwd/matchlist/pano_test.go
@@ -0,0 +1,38 @@
+package matchlist
+
+import (
+    "testing"
+    "reflect"
+
+    "github.com/PaloAltoNetworks/pango/testdata"
+)
+
+
+func TestPanoNormalization(t *testing.T) {
+    testCases := getTests()
+
+    mc := &testdata.MockClient{}
+    ns := &PanoMatchList{}
+    ns.Initialize(mc)
+
+    for _, tc := range testCases {
+        t.Run(tc.desc, func(t *testing.T) {
+            mc.Version = tc.version
+            mc.Reset()
+            mc.AddResp("")
+            err := ns.Set("", "", "", "shared", "lfp", tc.conf)
+            if err != nil {
+                t.Errorf("Error in set: %s", err)
+            } else {
+                mc.AddResp(mc.Elm)
+                r, err := ns.Get("", "", "", "shared", "lfp", tc.conf.Name)
+                if err != nil {
+                    t.Errorf("Error in get: %s", err)
+                }
+                if !reflect.DeepEqual(tc.conf, r) {
+                    t.Errorf("%#v != %#v", tc.conf, r)
+                }
+            }
+        })
+    }
+}
---
+++ objs/profile/logfwd/matchlist/testdata_test.go
@@ -0,0 +1,35 @@
+package matchlist
+
+import (
+    "github.com/PaloAltoNetworks/pango/version"
+)
+
+type tc struct {
+    desc string
+    version version.Number
+    conf Entry
+}
+
+func getTests() []tc {
+    return []tc{
+        {"basic without raw", version.Number{8, 0, 0, ""}, Entry{
+            Name: "t1",
+            Description: "foobar",
+            LogType: LogTypeTunnel,
+            Filter: "my filter",
+            SnmpProfiles: []string{"prof1", "prof2"},
+            SyslogProfiles: []string{"syslog1", "syslog2"},
+        }},
+        {"basic without raw", version.Number{8, 0, 0, ""}, Entry{
+            Name: "t1",
+            LogType: LogTypeAuth,
+            Filter: "my other filter",
+            SendToPanorama: true,
+            EmailProfiles: []string{"email1", "email2"},
+            HttpProfiles: []string{"http1", "http3"},
+            raw: map[string] string{
+                "act": "my various actions",
+            },
+        }},
+    }
+}
