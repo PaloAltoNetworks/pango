06a926c2e4f87e8f01798a186ce265f24f8232db
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-07-01 15:03:28 -0700

    adding ip-wildcard to address objects

--- objs/addr/fw_test.go
+++ objs/addr/fw_test.go
@@ -5,6 +5,7 @@ import (
     "reflect"
 
     "github.com/PaloAltoNetworks/pango/testdata"
+    "github.com/PaloAltoNetworks/pango/version"
 )
 
 
@@ -12,26 +13,53 @@ func TestFwNormalization(t *testing.T) {
     testCases := []struct{
         desc string
         vsys string
+        version version.Number
         conf Entry
     }{
-        {"test ip netmask", "", Entry{
+        {"v1 test ip netmask", "", version.Number{7, 1, 0, ""}, Entry{
             Name: "one",
             Value: "10.1.1.0/24",
             Type: IpNetmask,
             Description: "my description",
             Tags: []string{"tag1", "tag2"},
         }},
-        {"test ip range", "vsys2", Entry{
+        {"v1 test ip range", "vsys2", version.Number{7, 1, 0, ""}, Entry{
             Name: "two",
             Value: "10.1.1.1-10.1.1.254",
             Type: IpRange,
             Description: "my description",
             Tags: []string{"tag3", "tag4"},
         }},
-        {"test fqdn", "vsys3", Entry{
+        {"v1 test fqdn", "vsys3", version.Number{7, 1, 0, ""}, Entry{
             Name: "three",
             Value: "example.com",
             Type: Fqdn,
+            Description: "my description",
+        }},
+        {"v2 test ip netmask", "", version.Number{9, 0, 0, ""}, Entry{
+            Name: "one",
+            Value: "10.1.1.0/24",
+            Type: IpNetmask,
+            Description: "my description",
+            Tags: []string{"tag1", "tag2"},
+        }},
+        {"v2 test ip range", "vsys2", version.Number{9, 0, 0, ""}, Entry{
+            Name: "two",
+            Value: "10.1.1.1-10.1.1.254",
+            Type: IpRange,
+            Description: "my description",
+            Tags: []string{"tag3", "tag4"},
+        }},
+        {"v2 test fqdn", "vsys3", version.Number{9, 0, 0, ""}, Entry{
+            Name: "three",
+            Value: "example.com",
+            Type: Fqdn,
+            Description: "my description",
+        }},
+        {"v2 test ip wildcard", "vsys4", version.Number{9, 0, 0, ""}, Entry{
+            Name: "four",
+            Value: "10.20.1.0/0.0.248.255",
+            Type: IpWildcard,
             Description: "my description",
         }},
     }
@@ -42,6 +70,7 @@ func TestFwNormalization(t *testing.T) {
 
     for _, tc := range testCases {
         t.Run(tc.desc, func(t *testing.T) {
+            mc.Version = tc.version
             mc.Reset()
             mc.AddResp("")
             err := ns.Set(tc.vsys, tc.conf)
--- objs/addr/pano_test.go
+++ objs/addr/pano_test.go
@@ -5,6 +5,7 @@ import (
     "reflect"
 
     "github.com/PaloAltoNetworks/pango/testdata"
+    "github.com/PaloAltoNetworks/pango/version"
 )
 
 
@@ -12,26 +13,53 @@ func TestPanoNormalization(t *testing.T) {
     testCases := []struct{
         desc string
         dg string
+        version version.Number
         conf Entry
     }{
-        {"test ip netmask", "", Entry{
+        {"v1 test ip netmask", "", version.Number{7, 1, 0, ""}, Entry{
             Name: "one",
             Value: "10.1.1.0/24",
             Type: IpNetmask,
             Description: "my description",
             Tags: []string{"tag1", "tag2"},
         }},
-        {"test ip range", "dg1", Entry{
+        {"v1 test ip range", "dg1", version.Number{7, 1, 0, ""}, Entry{
             Name: "two",
             Value: "10.1.1.1-10.1.1.254",
             Type: IpRange,
             Description: "my description",
             Tags: []string{"tag3", "tag4"},
         }},
-        {"test fqdn", "dg2", Entry{
+        {"v1 test fqdn", "dg2", version.Number{7, 1, 0, ""}, Entry{
             Name: "three",
             Value: "example.com",
             Type: Fqdn,
+            Description: "my description",
+        }},
+        {"v2 test ip netmask", "", version.Number{9, 0, 0, ""}, Entry{
+            Name: "one",
+            Value: "10.1.1.0/24",
+            Type: IpNetmask,
+            Description: "my description",
+            Tags: []string{"tag1", "tag2"},
+        }},
+        {"v2 test ip range", "dg1", version.Number{9, 0, 0, ""}, Entry{
+            Name: "two",
+            Value: "10.1.1.1-10.1.1.254",
+            Type: IpRange,
+            Description: "my description",
+            Tags: []string{"tag3", "tag4"},
+        }},
+        {"v2 test fqdn", "dg2", version.Number{9, 0, 0, ""}, Entry{
+            Name: "three",
+            Value: "example.com",
+            Type: Fqdn,
+            Description: "my description",
+        }},
+        {"v2 test ip wildcard", "dg3", version.Number{9, 0, 0, ""}, Entry{
+            Name: "four",
+            Value: "10.20.1.0/0.0.248.255",
+            Type: IpWildcard,
             Description: "my description",
         }},
     }
@@ -42,6 +70,7 @@ func TestPanoNormalization(t *testing.T) {
 
     for _, tc := range testCases {
         t.Run(tc.desc, func(t *testing.T) {
+            mc.Version = tc.version
             mc.Reset()
             mc.AddResp("")
             err := ns.Set(tc.dg, tc.conf)
