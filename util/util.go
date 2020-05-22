a6e945b8098fd87729c5bb99d2b72c9969475abb
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-05-22 13:03:08 -0700

    updating job info struct to be able to get all the details returned from pan-os

--- util/util.go
+++ util/util.go
@@ -427,11 +427,36 @@ type JobResponse struct {
 // BasicJob is a struct for parsing minimal information about a submitted
 // job to PANOS.
 type BasicJob struct {
-	XMLName  xml.Name `xml:"response"`
-	Result   string   `xml:"result>job>result"`
-	Progress uint     `xml:"result>job>progress"`
-	Details  []string `xml:"result>job>details>line"`
-	Devices  []devJob `xml:"result>job>devices>entry"`
+	XMLName  xml.Name        `xml:"response"`
+	Result   string          `xml:"result>job>result"`
+	Progress uint            `xml:"result>job>progress"`
+	Details  BasicJobDetails `xml:"result>job>details"`
+	Devices  []devJob        `xml:"result>job>devices>entry"`
+}
+
+type BasicJobDetails struct {
+	Lines []LineOrCdata `xml:"line"`
+}
+
+func (o *BasicJobDetails) String() string {
+	ans := make([]string, 0, len(o.Lines))
+
+	for _, line := range o.Lines {
+		if line.Cdata != nil {
+			ans = append(ans, strings.TrimSpace(*line.Cdata))
+		} else if line.Text != nil {
+			ans = append(ans, *line.Text)
+		} else {
+			ans = append(ans, "huh")
+		}
+	}
+
+	return strings.Join(ans, " | ")
+}
+
+type LineOrCdata struct {
+	Cdata *string `xml:",cdata"`
+	Text  *string `xml:",chardata"`
 }
 
 // Internally used by BasicJob for panorama commit-all.
