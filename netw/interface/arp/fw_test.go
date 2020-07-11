1ca182dc4f546dab2645175c206eb4dbab3393a9
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-07-10 23:01:36 -0700

    use testdata

--- netw/interface/arp/fw_test.go
+++ netw/interface/arp/fw_test.go
@@ -8,35 +8,7 @@ import (
 )
 
 func TestFwNormalization(t *testing.T) {
-	testCases := []struct {
-		desc    string
-		iType   string
-		iName   string
-		subName string
-		conf    Entry
-	}{
-		{"eth no sub", TypeEthernet, "ethernet1/5", "", Entry{
-			Ip:         "10.1.1.1",
-			MacAddress: "00:30:48:52:ab:c1",
-		}},
-		{"eth with sub", TypeEthernet, "ethernet1/5", "ethernet1/5.7", Entry{
-			Ip:         "10.1.1.2",
-			MacAddress: "00:30:48:52:ab:c2",
-		}},
-		{"agg no sub", TypeAggregate, "ae5", "", Entry{
-			Ip:         "10.1.1.3",
-			MacAddress: "00:30:48:52:ab:c3",
-		}},
-		{"agg with sub", TypeAggregate, "ae5", "ae5.4", Entry{
-			Ip:         "10.1.1.4",
-			MacAddress: "00:30:48:52:ab:c4",
-		}},
-		{"vlan", TypeVlan, "", "vlan.4", Entry{
-			Ip:         "10.1.1.5",
-			MacAddress: "00:30:48:52:ab:c5",
-			Interface:  "vlan.2",
-		}},
-	}
+	testCases := getTests()
 
 	mc := &testdata.MockClient{}
 	ns := &FwArp{}
