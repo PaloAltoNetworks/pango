7956992f539d0fd1bc917685a224114914f42154
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-07-10 20:34:36 -0700

    update to use namespace code

--- poli/security/pano.go
+++ poli/security/pano.go
@@ -1,107 +1,92 @@
 package security
 
 import (
-	"encoding/xml"
 	"fmt"
 	"strings"
 
+	"github.com/PaloAltoNetworks/pango/namespace"
 	"github.com/PaloAltoNetworks/pango/util"
 )
 
 // PanoSecurity is the client.Policies.Security namespace.
 type PanoSecurity struct {
 	con util.XapiClient
+	ns  *namespace.Namespace
 }
 
 // Initialize is invoed by client.Initialize().
 func (c *PanoSecurity) Initialize(con util.XapiClient) {
 	c.con = con
+	c.ns = namespace.New(singular, plural, con)
 }
 
-// GetList performs GET to retrieve a list of security policies.
+// GetList performs GET to retrieve a list of object names.
 func (c *PanoSecurity) GetList(dg, base string) ([]string, error) {
-	c.con.LogQuery("(get) list of security policies")
-	path := c.xpath(dg, base, nil)
-	return c.con.EntryListUsing(c.con.Get, path[:len(path)-1])
+	result, _ := c.versioning()
+	return c.ns.Listing(util.Get, c.xpath(dg, base, nil), result)
 }
 
-// ShowList performs SHOW to retrieve a list of security policies.
+// ShowList performs SHOW to retrieve a list of object names.
 func (c *PanoSecurity) ShowList(dg, base string) ([]string, error) {
-	c.con.LogQuery("(show) list of security policies")
-	path := c.xpath(dg, base, nil)
-	return c.con.EntryListUsing(c.con.Show, path[:len(path)-1])
+	result, _ := c.versioning()
+	return c.ns.Listing(util.Show, c.xpath(dg, base, nil), result)
 }
 
-// Get performs GET to retrieve information for the given security policy.
+// Get performs GET to retrieve information for the given object.
 func (c *PanoSecurity) Get(dg, base, name string) (Entry, error) {
-	c.con.LogQuery("(get) security policy %q", name)
-	listing, err := c.details(c.con.Get, dg, base, name)
-	if err == nil && len(listing) > 0 {
-		return listing[0], nil
+	result, _ := c.versioning()
+	if err := c.ns.Object(util.Get, c.xpath(dg, base, []string{name}), name, result); err != nil {
+		return Entry{}, err
 	}
-	return Entry{}, err
+
+	return result.Normalize()[0], nil
 }
 
-// GetAll performs GET to retrieve all security policies.
+// GetAll performs GET to retrieve all objects.
 func (c *PanoSecurity) GetAll(dg, base string) ([]Entry, error) {
-	c.con.LogQuery("(get) all security policies")
-	return c.details(c.con.Get, dg, base, "")
-}
-
-// Get performs SHOW to retrieve information for the given security policy.
-func (c *PanoSecurity) Show(dg, base, name string) (Entry, error) {
-	c.con.LogQuery("(show) security policy %q", name)
-	listing, err := c.details(c.con.Show, dg, base, name)
-	if err == nil && len(listing) > 0 {
-		return listing[0], nil
+	result, _ := c.versioning()
+	if err := c.ns.Objects(util.Get, c.xpath(dg, base, nil), result); err != nil {
+		return nil, err
 	}
-	return Entry{}, err
+
+	return result.Normalize(), nil
 }
 
-// ShowAll performs SHOW to retrieve all security policies.
+// Show performs SHOW to retrieve information for the given object.
+func (c *PanoSecurity) Show(dg, base, name string) (Entry, error) {
+	result, _ := c.versioning()
+	if err := c.ns.Object(util.Show, c.xpath(dg, base, []string{name}), name, result); err != nil {
+		return Entry{}, err
+	}
+
+	return result.Normalize()[0], nil
+}
+
+// ShowAll performs SHOW to retrieve all objects.
 func (c *PanoSecurity) ShowAll(dg, base string) ([]Entry, error) {
-	c.con.LogQuery("(show) all security policies")
-	return c.details(c.con.Show, dg, base, "")
+	result, _ := c.versioning()
+	if err := c.ns.Objects(util.Show, c.xpath(dg, base, nil), result); err != nil {
+		return nil, err
+	}
+
+	return result.Normalize(), nil
 }
 
 // Set performs SET to create / update one or more security policies.
 func (c *PanoSecurity) Set(dg, base string, e ...Entry) error {
 	var err error
 
-	if len(e) == 0 {
-		return nil
-	} else {
-		// Make sure rule names are unique.
-		m := make(map[string]int)
-		for i := range e {
-			m[e[i].Name] = m[e[i].Name] + 1
-			if m[e[i].Name] > 1 {
-				return fmt.Errorf("Security rule is defined multiple times: %s", e[i].Name)
-			}
-		}
-	}
-
 	_, fn := c.versioning()
+	data := make([]interface{}, 0, len(e))
 	names := make([]string, len(e))
 
-	// Build up the struct with the given security policy configs.
-	d := util.BulkElement{XMLName: xml.Name{Local: "rules"}}
 	for i := range e {
-		d.Data = append(d.Data, fn(e[i]))
-		names[i] = e[i].Name
+		data = append(data, fn(e[i]))
+		names = append(names, e[i].Name)
 	}
-	c.con.LogAction("(set) security policies: %v", names)
-
-	// Set xpath.
 	path := c.xpath(dg, base, names)
-	if len(e) == 1 {
-		path = path[:len(path)-1]
-	} else {
-		path = path[:len(path)-2]
-	}
 
-	// Create the security policies.
-	_, err = c.con.Set(path, d.Config(), nil, nil)
+	err = c.ns.Set(names, path, data)
 
 	// On error: find the rule that's causing the error if multiple rules
 	// were given.
@@ -153,18 +138,11 @@ func (c *PanoSecurity) VerifiableSet(dg, base string, e ...Entry) error {
 
 // Edit performs EDIT to create / update a security policy.
 func (c *PanoSecurity) Edit(dg, base string, e Entry) error {
-	var err error
-
 	_, fn := c.versioning()
-
-	c.con.LogAction("(edit) security policy %q", e.Name)
-
-	// Set xpath.
 	path := c.xpath(dg, base, []string{e.Name})
+	data := fn(e)
 
-	// Edit the security policy.
-	_, err = c.con.Edit(path, fn(e), nil, nil)
-	return err
+	return c.ns.Edit(e.Name, path, data)
 }
 
 // VerifiableEdit behaves like Edit(), except policies with LogEnd as true
@@ -205,28 +183,20 @@ func (c *PanoSecurity) VerifiableEdit(dg, base string, e ...Entry) error {
 //
 // Security policies can be either a string or an Entry object.
 func (c *PanoSecurity) Delete(dg, base string, e ...interface{}) error {
-	var err error
-
-	if len(e) == 0 {
-		return nil
-	}
-
-	names := make([]string, len(e))
+	names := make([]string, 0, len(e))
 	for i := range e {
 		switch v := e[i].(type) {
 		case string:
-			names[i] = v
+			names = append(names, v)
 		case Entry:
-			names[i] = v.Name
+			names = append(names, v.Name)
 		default:
 			return fmt.Errorf("Unsupported type to delete: %s", v)
 		}
 	}
-	c.con.LogAction("(delete) security policies: %v", names)
 
 	path := c.xpath(dg, base, names)
-	_, err = c.con.Delete(path, nil, nil)
-	return err
+	return c.ns.Delete(names, path)
 }
 
 // DeleteAll removes all security policies from the specified dg / rulebase.
@@ -254,109 +224,26 @@ func (c *PanoSecurity) DeleteAll(dg, base string) error {
 // anywhere, but all other policies will still be moved to be grouped with the
 // first one.
 func (c *PanoSecurity) MoveGroup(dg, base string, movement int, rule string, e ...Entry) error {
-	var err error
-
-	fIdx := -1
-	oIdx := -1
-
-	c.con.LogAction("(move) security policy group")
-	if len(e) < 1 {
-		return fmt.Errorf("Requires at least one security policy")
-	} else if rule == e[0].Name {
-		return fmt.Errorf("Can't position %q in relation to itself", rule)
-	} else if movement != util.MoveSkip && movement != util.MoveBefore && movement != util.MoveDirectlyBefore && movement != util.MoveAfter && movement != util.MoveDirectlyAfter && movement != util.MoveTop && movement != util.MoveBottom {
-		return fmt.Errorf("Invalid position specified: %d", movement)
-	} else if (movement == util.MoveBefore || movement == util.MoveDirectlyBefore || movement == util.MoveAfter || movement == util.MoveDirectlyAfter) && rule == "" {
-		return fmt.Errorf("Specify 'rule' in order to perform relative group positioning")
-	}
-	path := c.xpath(dg, base, []string{e[0].Name})
-
-	if movement != util.MoveSkip {
-		// Get the list of current security policies.
-		curList, err := c.GetList(dg, base)
-		if err != nil {
-			return err
-		} else if len(curList) == 0 {
-			return fmt.Errorf("No policies found")
-		}
-
-		switch movement {
-		case util.MoveTop:
-			_, em := c.con.Move(path, "top", "", nil, nil)
-			if em != nil {
-				if em.Error() != "already at the top" {
-					err = em
-				}
-			}
-		case util.MoveBottom:
-			_, em := c.con.Move(path, "bottom", "", nil, nil)
-			if em != nil {
-				if em.Error() != "already at the bottom" {
-					err = em
-				}
-			}
-		default:
-			// Find the indexes of the first security policy and the ref policy.
-			for i, v := range curList {
-				if v == e[0].Name {
-					fIdx = i
-				} else if v == rule {
-					oIdx = i
-				}
-				if fIdx != -1 && oIdx != -1 {
-					break
-				}
-			}
-
-			// Sanity check:  both rules should be present.
-			if fIdx == -1 {
-				return fmt.Errorf("First security policy in group %q does not exist", e[0].Name)
-			} else if oIdx == -1 {
-				return fmt.Errorf("Reference security policy %q does not exist", rule)
-			}
-
-			// Perform the move of the first security policy, if needed.
-			if (movement == util.MoveBefore && fIdx > oIdx) || (movement == util.MoveDirectlyBefore && fIdx+1 != oIdx) {
-				_, err = c.con.Move(path, "before", rule, nil, nil)
-			} else if (movement == util.MoveAfter && fIdx < oIdx) || (movement == util.MoveDirectlyAfter && fIdx != oIdx+1) {
-				_, err = c.con.Move(path, "after", rule, nil, nil)
-			}
-		}
-
-		// If we moved something, make sure it worked.
-		if err != nil {
-			return err
-		}
+	pather := func(v string) []string {
+		return c.xpath(dg, base, []string{v})
 	}
 
-	// Now move all other rules under the first.
-	li := len(path) - 1
-	for i := 1; i < len(e); i++ {
-		path[li] = util.AsEntryXpath([]string{e[i].Name})
-		_, err = c.con.Move(path, "after", e[i-1].Name, nil, nil)
-		if err != nil {
-			return err
-		}
+	lister := func() ([]string, error) {
+		return c.GetList(dg, base)
 	}
 
-	return nil
+	names := make([]string, 0, len(e))
+	for i := range e {
+		names = append(names, e[i].Name)
+	}
+
+	return c.ns.MoveGroup(pather, lister, movement, rule, names)
 }
 
 /** Internal functions for the PanoSecurity struct **/
 
 func (c *PanoSecurity) versioning() (normalizer, func(Entry) interface{}) {
 	return &container_v1{}, specify_v1
-}
-
-func (c *PanoSecurity) details(fn util.Retriever, dg, base, name string) ([]Entry, error) {
-	path := c.xpath(dg, base, []string{name})
-	obj, _ := c.versioning()
-	if _, err := fn(path, nil, obj); err != nil {
-		return nil, err
-	}
-	ans := obj.Normalize()
-
-	return ans, nil
 }
 
 func (c *PanoSecurity) xpath(dg, base string, vals []string) []string {
