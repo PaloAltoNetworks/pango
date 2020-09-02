ba515c73d5ad376eb5a998c12f80bc3bc93187c5
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-09-02 14:59:15 -0700

    refactoring unittests for loopback interfaces

--- netw/interface/loopback/pano_test.go
+++ netw/interface/loopback/pano_test.go
@@ -5,56 +5,10 @@ import (
 	"testing"
 
 	"github.com/PaloAltoNetworks/pango/testdata"
-	"github.com/PaloAltoNetworks/pango/version"
 )
 
 func TestPanoNormalization(t *testing.T) {
-	testCases := []struct {
-		version    version.Number
-		tmpl       string
-		vsys       string
-		importVsys string
-		imports    []string
-		conf       Entry
-	}{
-		{version.Number{5, 0, 0, ""}, "one", "vsys2", "vsys2", []string{"loopback.1"}, Entry{
-			Name:              "loopback.1",
-			StaticIps:         []string{"10.1.1.1/24", "10.2.1.1/24"},
-			ManagementProfile: "enable ping",
-			Mtu:               1500,
-			AdjustTcpMss:      true,
-			NetflowProfile:    "some profile",
-			Comment:           "v1 basic",
-		}},
-		{version.Number{5, 0, 0, ""}, "two", "vsys3", "vsys3", []string{"loopback.2"}, Entry{
-			Name:      "loopback.2",
-			StaticIps: []string{"10.3.1.1/24"},
-			raw: map[string]string{
-				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
-			},
-			Comment: "v1 raw no import",
-		}},
-		{version.Number{8, 0, 0, ""}, "three", "vsys2", "vsys2", []string{"loopback.3"}, Entry{
-			Name:              "loopback.3",
-			StaticIps:         []string{"10.1.1.1/24", "10.2.1.1/24"},
-			ManagementProfile: "enable ping",
-			Mtu:               1500,
-			AdjustTcpMss:      true,
-			Ipv4MssAdjust:     12,
-			Ipv6MssAdjust:     14,
-			NetflowProfile:    "some profile",
-			Comment:           "v2 basic",
-		}},
-		{version.Number{8, 0, 0, ""}, "four", "vsys3", "vsys3", []string{"loopback.4"}, Entry{
-			Name:          "loopback.4",
-			StaticIps:     []string{"10.3.1.1/24"},
-			Ipv6MssAdjust: 1024,
-			raw: map[string]string{
-				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
-			},
-			Comment: "v2 raw no import",
-		}},
-	}
+	testCases := getTests()
 
 	mc := &testdata.MockClient{}
 	ns := &PanoLoopback{}
@@ -62,6 +16,10 @@ func TestPanoNormalization(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.conf.Comment, func(t *testing.T) {
+			if tc.tmpl == "" {
+				t.Skip()
+			}
+
 			var err error
 			mc.Version = tc.version
 			mc.Reset()
---
+++ netw/interface/loopback/testdata_test.go
@@ -0,0 +1,73 @@
+package loopback
+
+import (
+	"github.com/PaloAltoNetworks/pango/version"
+)
+
+type testCase struct {
+	version    version.Number
+	tmpl       string
+	vsys       string
+	importVsys string
+	imports    []string
+	conf       Entry
+}
+
+func getTests() []testCase {
+	return []testCase{
+		{version.Number{5, 0, 0, ""}, "one", "vsys2", "vsys2", []string{"loopback.1"}, Entry{
+			Name:              "loopback.1",
+			StaticIps:         []string{"10.1.1.1/24", "10.2.1.1/24"},
+			ManagementProfile: "enable ping",
+			Mtu:               1500,
+			AdjustTcpMss:      true,
+			NetflowProfile:    "some profile",
+			Comment:           "v1 basic",
+		}},
+		{version.Number{5, 0, 0, ""}, "two", "vsys3", "vsys3", []string{"loopback.2"}, Entry{
+			Name:      "loopback.2",
+			StaticIps: []string{"10.3.1.1/24"},
+			raw: map[string]string{
+				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
+			},
+			Comment: "v1 raw no import",
+		}},
+		{version.Number{5, 0, 0, ""}, "", "", "", []string{"loopback.2"}, Entry{
+			Name:      "loopback.2",
+			StaticIps: []string{"10.3.1.1/24"},
+			raw: map[string]string{
+				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
+			},
+			Comment: "v1 tmpl with raw",
+		}},
+		{version.Number{8, 0, 0, ""}, "three", "vsys2", "vsys2", []string{"loopback.3"}, Entry{
+			Name:              "loopback.3",
+			StaticIps:         []string{"10.1.1.1/24", "10.2.1.1/24"},
+			ManagementProfile: "enable ping",
+			Mtu:               1500,
+			AdjustTcpMss:      true,
+			Ipv4MssAdjust:     12,
+			Ipv6MssAdjust:     14,
+			NetflowProfile:    "some profile",
+			Comment:           "v2 basic",
+		}},
+		{version.Number{8, 0, 0, ""}, "", "", "", []string{"loopback.4"}, Entry{
+			Name:          "loopback.4",
+			StaticIps:     []string{"10.3.1.1/24"},
+			Ipv6MssAdjust: 1024,
+			raw: map[string]string{
+				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
+			},
+			Comment: "v2 raw no import",
+		}},
+		{version.Number{8, 0, 0, ""}, "four", "vsys3", "vsys3", []string{"loopback.4"}, Entry{
+			Name:          "loopback.4",
+			StaticIps:     []string{"10.3.1.1/24"},
+			Ipv6MssAdjust: 1024,
+			raw: map[string]string{
+				"ipv6": "<ipv6>raw ipv6 addresses</ipv6>",
+			},
+			Comment: "v2 tmpl with raw",
+		}},
+	}
+}
