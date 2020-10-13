dffaf69ddf7b77bfc0dbc16c629b8f2b89179fe2
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-10-12 20:48:46 -0700

    use new eth.Firewall for fw

--- netw/interface/eth/fw.go
+++ netw/interface/eth/fw.go
@@ -1,216 +1,80 @@
 package eth
 
 import (
-	"encoding/xml"
-	"fmt"
-
 	"github.com/PaloAltoNetworks/pango/namespace"
 	"github.com/PaloAltoNetworks/pango/util"
-	"github.com/PaloAltoNetworks/pango/version"
 )
 
-// FwEth is the client.Network.EthernetInterface namespace.
-type FwEth struct {
-	con util.XapiClient
-	ns  *namespace.Namespace
+// Firewall is the client.Network.EthernetInterface namespace.
+type Firewall struct {
+	ns *namespace.Importable
 }
 
-// Initialize is invoked by client.Initialize().
-func (c *FwEth) Initialize(con util.XapiClient) {
-	c.con = con
-	c.ns = namespace.New(singular, plural, con)
+// GetList performs GET to retrieve a list of all objects.
+func (c *Firewall) GetList() ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Get, c.pather(), ans)
 }
 
-// ShowList performs SHOW to retrieve a list of ethernet interfaces.
-func (c *FwEth) ShowList() ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Show, c.xpath(nil), result)
+// ShowList performs a SHOW to retrieve a list of all objects.
+func (c *Firewall) ShowList() ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Show, c.pather(), ans)
 }
 
-// GetList performs GET to retrieve a list of ethernet interfaces.
-func (c *FwEth) GetList() ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Get, c.xpath(nil), result)
+// Get performs GET to retrieve configuration for the given object.
+func (c *Firewall) Get(name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Get, c.pather(), name, ans)
+	return first(ans, err)
 }
 
-// Get performs GET to retrieve information for the given ethernet interface.
-func (c *FwEth) Get(name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Get, c.xpath([]string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// Show performs SHOW to retrieve configuration for the given object.
+func (c *Firewall) Show(name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Show, c.pather(), name, ans)
+	return first(ans, err)
 }
 
-// GetAll perofrms GET to retrieve information for all objects.
-func (c *FwEth) GetAll() ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Get, c.xpath(nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// GetAll performs GET to retrieve all objects configured.
+func (c *Firewall) GetAll() ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Get, c.pather(), ans)
+	return all(ans, err)
 }
 
-// Show performs SHOW to retrieve information for the given ethernet interface.
-func (c *FwEth) Show(name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Show, c.xpath([]string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// ShowAll performs SHOW to retrieve all objects configured.
+func (c *Firewall) ShowAll() ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Show, c.pather(), ans)
+	return all(ans, err)
 }
 
-// ShowAll perofrms SHOW to retrieve information for all objects.
-func (c *FwEth) ShowAll() ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Show, c.xpath(nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// Set performs SET to configure the specified objects.
+func (c *Firewall) Set(vsys string, e ...Entry) error {
+	return c.ns.Set("", "", vsys, c.pather(), specifier(e...))
 }
 
-// Set performs SET to create / update one or more ethernet interfaces.
+// Edit performs EDIT to configure the specified object.
+func (c *Firewall) Edit(vsys string, e Entry) error {
+	return c.ns.Edit("", "", vsys, c.pather(), e)
+}
+
+// Delete performs DELETE to remove the specified objects.
 //
-// Specifying a non-empty vsys will import the interfaces into that vsys,
-// allowing the vsys to use them, as long as the interface does not have a
-// mode of "ha" or "aggregate-group".  Interfaces that have either of those
-// modes are omitted from this function's followup vsys import.
-func (c *FwEth) Set(vsys string, e ...Entry) error {
-	var err error
-
-	if len(e) == 0 {
-		return nil
-	}
-
-	_, fn := c.versioning()
-	n1 := make([]string, len(e))
-	n2 := make([]string, 0, len(e))
-
-	// Build up the struct with the given interface configs.
-	d := util.BulkElement{XMLName: xml.Name{Local: "ethernet"}}
-	for i := range e {
-		d.Data = append(d.Data, fn(e[i]))
-		n1[i] = e[i].Name
-		if e[i].Mode != "ha" && e[i].Mode != "aggregate-group" {
-			n2 = append(n2, e[i].Name)
-		}
-	}
-	c.con.LogAction("(set) %s: %v", plural, n1)
-
-	// Set xpath.
-	path := c.xpath(n1)
-	if len(e) == 1 {
-		path = path[:len(path)-1]
-	} else {
-		path = path[:len(path)-2]
-	}
-
-	// Create the interfaces.
-	_, err = c.con.Set(path, d.Config(), nil, nil)
-	if err != nil {
-		return err
-	}
-
-	// Remove the interfaces from any vsys they're currently in.
-	if err = c.con.VsysUnimport(util.InterfaceImport, "", "", n1); err != nil {
-		return err
-	}
-
-	// Perform vsys import next.
-	return c.con.VsysImport(util.InterfaceImport, "", "", vsys, n2)
+// Objects can be either a string or an Entry object.
+func (c *Firewall) Delete(e ...interface{}) error {
+	names, nErr := toNames(e)
+	return c.ns.Delete("", "", c.pather(), names, nErr)
 }
 
-// Edit performs EDIT to create / update the specified ethernet interface.
-//
-// Specifying a non-empty vsys will import the interface into that vsys,
-// allowing the vsys to use it, as long as the interface does not have a
-// mode of "ha" or "aggregate-group".  Interfaces that have either of those
-// modes are omitted from this function's followup vsys import.
-func (c *FwEth) Edit(vsys string, e Entry) error {
-	var err error
-
-	_, fn := c.versioning()
-
-	c.con.LogAction("(edit) %s: %q", singular, e.Name)
-
-	// Set xpath.
-	path := c.xpath([]string{e.Name})
-
-	// Edit the interface.
-	_, err = c.con.Edit(path, fn(e), nil, nil)
-	if err != nil {
-		return err
-	}
-
-	// Remove the interface from any vsys it's currently in.
-	if err = c.con.VsysUnimport(util.InterfaceImport, "", "", []string{e.Name}); err != nil {
-		return err
-	}
-
-	// Check if we should skip the import step.
-	if e.Mode == "ha" || e.Mode == "aggregate-group" {
-		return nil
-	}
-
-	// Import the interface.
-	return c.con.VsysImport(util.InterfaceImport, "", "", vsys, []string{e.Name})
-}
-
-// Delete removes the given interface(s) from the firewall.
-//
-// Interfaces can be a string or an Entry object.
-func (c *FwEth) Delete(e ...interface{}) error {
-	var err error
-
-	if len(e) == 0 {
-		return nil
-	}
-
-	names := make([]string, len(e))
-	for i := range e {
-		switch v := e[i].(type) {
-		case string:
-			names[i] = v
-		case Entry:
-			names[i] = v.Name
-		default:
-			return fmt.Errorf("Unknown type sent to delete: %s", v)
-		}
-	}
-	c.con.LogAction("(delete) %s: %v", plural, names)
-
-	// Unimport interfaces.
-	if err = c.con.VsysUnimport(util.InterfaceImport, "", "", names); err != nil {
-		return err
-	}
-
-	// Remove interfaces next.
-	path := c.xpath(names)
-	_, err = c.con.Delete(path, nil, nil)
-	return err
-}
-
-/** Internal functions for this namespace struct **/
-
-func (c *FwEth) versioning() (normalizer, func(Entry) interface{}) {
-	v := c.con.Versioning()
-
-	if v.Gte(version.Number{9, 0, 0, ""}) {
-		return &container_v4{}, specify_v4
-	} else if v.Gte(version.Number{8, 1, 0, ""}) {
-		return &container_v3{}, specify_v3
-	} else if v.Gte(version.Number{7, 1, 0, ""}) {
-		return &container_v2{}, specify_v2
-	} else {
-		return &container_v1{}, specify_v1
+func (c *Firewall) pather() namespace.Pather {
+	return func(v []string) ([]string, error) {
+		return c.xpath(v)
 	}
 }
 
-func (c *FwEth) xpath(vals []string) []string {
+func (c *Firewall) xpath(vals []string) ([]string, error) {
 	return []string{
 		"config",
 		"devices",
@@ -219,5 +83,9 @@ func (c *FwEth) xpath(vals []string) []string {
 		"interface",
 		"ethernet",
 		util.AsEntryXpath(vals),
-	}
+	}, nil
+}
+
+func (c *Firewall) container() normalizer {
+	return container(c.ns.Client.Versioning())
 }
