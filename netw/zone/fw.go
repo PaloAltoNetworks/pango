75bfdf3770c53902b0bb5cb5ae775b9aac09f414
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-10-15 11:09:13 -0700

    namespace update: zones

--- netw/zone/fw.go
+++ netw/zone/fw.go
@@ -7,16 +7,9 @@ import (
 	"github.com/PaloAltoNetworks/pango/util"
 )
 
-// FwZone is a namespace struct, included as part of pango.Client.
-type FwZone struct {
-	con util.XapiClient
-	ns  *namespace.Namespace
-}
-
-// Initialize is invoked when Initialize on the pango.Client is called.
-func (c *FwZone) Initialize(con util.XapiClient) {
-	c.con = con
-	c.ns = namespace.New(singular, plural, con)
+// Firewall is the client.Network.Zone namespace.
+type Firewall struct {
+	ns *namespace.Standard
 }
 
 /*
@@ -24,24 +17,28 @@ SetInterface performs a SET to add an interface to a zone.
 
 The zone can be either a string or an Entry object.
 */
-func (c *FwZone) SetInterface(vsys string, zone interface{}, mode, iface string) error {
-	var name string
+func (c *Firewall) SetInterface(vsys string, zone interface{}, mode, iface string) error {
+	names, err := toNames([]interface{}{zone})
+	if err != nil {
+		return err
+	}
+	name := names[0]
 
-	switch v := zone.(type) {
-	case string:
-		name = v
-	case Entry:
-		name = v.Name
+	switch mode {
+	case ModeL2, ModeL3, ModeVirtualWire, ModeTap, ModeExternal:
 	default:
-		return fmt.Errorf("Unknown type sent to %s set interface: %s", singular, v)
+		return fmt.Errorf("unknown mode value: %s", mode)
 	}
 
-	c.con.LogAction("(set) %s interface: %s", singular, name)
+	c.ns.Client.LogAction("(set) %s interface: %s", singular, name)
 
-	path := c.xpath(vsys, []string{name})
+	path, err := c.xpath(vsys, []string{name})
+	if err != nil {
+		return err
+	}
 	path = append(path, "network", mode)
 
-	_, err := c.con.Set(path, util.Member{Value: iface}, nil, nil)
+	_, err = c.ns.Client.Set(path, util.Member{Value: iface}, nil, nil)
 	return err
 }
 
@@ -50,141 +47,110 @@ DeleteInterface performs a DELETE to remove the interface from the zone.
 
 The zone can be either a string or an Entry object.
 */
-func (c *FwZone) DeleteInterface(vsys string, zone interface{}, mode, iface string) error {
-	var name string
+func (c *Firewall) DeleteInterface(vsys string, zone interface{}, mode, iface string) error {
+	names, err := toNames([]interface{}{zone})
+	if err != nil {
+		return err
+	}
+	name := names[0]
 
-	switch v := zone.(type) {
-	case string:
-		name = v
-	case Entry:
-		name = v.Name
+	switch mode {
+	case ModeL2, ModeL3, ModeVirtualWire, ModeTap, ModeExternal:
 	default:
-		return fmt.Errorf("Unknown type sent to %s delete interface: %s", singular, v)
+		return fmt.Errorf("unknown mode value: %s", mode)
 	}
 
-	c.con.LogAction("(delete) %s interface: %s", singular, name)
+	c.ns.Client.LogAction("(delete) %s interface: %s", singular, name)
 
-	path := c.xpath(vsys, []string{name})
+	path, err := c.xpath(vsys, []string{name})
+	if err != nil {
+		return err
+	}
 	path = append(path, "network", mode, util.AsMemberXpath([]string{iface}))
 
-	_, err := c.con.Delete(path, nil, nil)
+	_, err = c.ns.Client.Delete(path, nil, nil)
 	return err
 }
 
-// GetList performs GET to retrieve a list of values.
-func (c *FwZone) GetList(vsys string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Get, c.xpath(vsys, nil), result)
+// GetList performs GET to retrieve a list of all objects.
+func (c *Firewall) GetList(vsys string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Get, c.pather(vsys), ans)
 }
 
-// ShowList performs SHOW to retrieve a list of values.
-func (c *FwZone) ShowList(vsys string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Show, c.xpath(vsys, nil), result)
+// ShowList performs SHOW to retrieve a list of all objects.
+func (c *Firewall) ShowList(vsys string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Show, c.pather(vsys), ans)
 }
 
-// Get performs GET to retrieve information for the given uid.
-func (c *FwZone) Get(vsys, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Get, c.xpath(vsys, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// Get performs GET to retrieve information for the given object.
+func (c *Firewall) Get(vsys, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Get, c.pather(vsys), name, ans)
+	return first(ans, err)
 }
 
-// GetAll performs GET to retrieve information for all objects.
-func (c *FwZone) GetAll(vsys string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Get, c.xpath(vsys, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// Show performs SHOW to retrieve information for the given object.
+func (c *Firewall) Show(vsys, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Show, c.pather(vsys), name, ans)
+	return first(ans, err)
 }
 
-// Get performs SHOW to retrieve information for the given uid.
-func (c *FwZone) Show(vsys, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Show, c.xpath(vsys, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// GetAll performs GET to retrieve all objects configured.
+func (c *Firewall) GetAll(vsys string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Get, c.pather(vsys), ans)
+	return all(ans, err)
 }
 
 // ShowAll performs SHOW to retrieve information for all objects.
-func (c *FwZone) ShowAll(vsys string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Show, c.xpath(vsys, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+func (c *Firewall) ShowAll(vsys string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Show, c.pather(vsys), ans)
+	return all(ans, err)
 }
 
-// Set performs SET to create / update one or more objects.
-func (c *FwZone) Set(vsys string, e ...Entry) error {
-	_, fn := c.versioning()
-	data := make([]interface{}, 0, len(e))
-	names := make([]string, 0, len(e))
-
-	for i := range e {
-		data = append(data, fn(e[i]))
-		names = append(names, e[i].Name)
-	}
-	path := c.xpath(vsys, names)
-
-	return c.ns.Set(names, path, data)
+// Set performs SET to configure the specified objects.
+func (c *Firewall) Set(vsys string, e ...Entry) error {
+	return c.ns.Set(c.pather(vsys), specifier(e...))
 }
 
-// Edit performs EDIT to create / update one object.
-func (c *FwZone) Edit(vsys string, e Entry) error {
-	_, fn := c.versioning()
-	path := c.xpath(vsys, []string{e.Name})
-	data := fn(e)
-
-	return c.ns.Edit(e.Name, path, data)
+// Edit performs EDIT to configure the specified object.
+func (c *Firewall) Edit(vsys string, e Entry) error {
+	return c.ns.Edit(c.pather(vsys), e)
 }
 
-// Delete removes the given objects.
+// Delete performs DELETE to remove the specified objects.
 //
 // Objects can be either a string or an Entry object.
-func (c *FwZone) Delete(vsys string, e ...interface{}) error {
-	names := make([]string, len(e))
-	for i := range e {
-		switch v := e[i].(type) {
-		case string:
-			names[i] = v
-		case Entry:
-			names[i] = v.Name
-		default:
-			return fmt.Errorf("Unsupported type to delete: %s", v)
-		}
-	}
-
-	path := c.xpath(vsys, names)
-	return c.ns.Delete(names, path)
+func (c *Firewall) Delete(vsys string, e ...interface{}) error {
+	names, nErr := toNames(e)
+	return c.ns.Delete(c.pather(vsys), names, nErr)
 }
 
-/** Internal functions for this namespace struct **/
-
-func (c *FwZone) versioning() (normalizer, func(Entry) interface{}) {
-	return &container_v1{}, specify_v1
+func (c *Firewall) pather(vsys string) namespace.Pather {
+	return func(v []string) ([]string, error) {
+		return c.xpath(vsys, v)
+	}
 }
 
-func (c *FwZone) xpath(vsys string, vals []string) []string {
-	if vsys == "" {
-		vsys = "vsys1"
+func (c *Firewall) xpath(vsys string, vals []string) ([]string, error) {
+	if vsys == "shared" {
+		return nil, fmt.Errorf("vsys cannot be 'shared'")
 	}
 
-	return []string{
-		"config",
-		"devices",
-		util.AsEntryXpath([]string{"localhost.localdomain"}),
-		"vsys",
-		util.AsEntryXpath([]string{vsys}),
+	ans := make([]string, 0, 7)
+	ans = append(ans, util.VsysXpathPrefix(vsys)...)
+	ans = append(ans,
 		"zone",
 		util.AsEntryXpath(vals),
-	}
+	)
+
+	return ans, nil
+}
+
+func (c *Firewall) container() normalizer {
+	return container(c.ns.Client.Versioning())
 }
--- netw/zone/pano.go
+++ netw/zone/pano.go
@@ -7,16 +7,9 @@ import (
 	"github.com/PaloAltoNetworks/pango/util"
 )
 
-// PanoZone is a namespace struct, included as part of pango.Client.
-type PanoZone struct {
-	con util.XapiClient
-	ns  *namespace.Namespace
-}
-
-// Initialize is invoked when Initialize on the pango.Client is called.
-func (c *PanoZone) Initialize(con util.XapiClient) {
-	c.con = con
-	c.ns = namespace.New(singular, plural, con)
+// Panorama is the client.Network.Zone namespace.
+type Panorama struct {
+	ns *namespace.Standard
 }
 
 /*
@@ -24,28 +17,28 @@ SetInterface performs a SET to add an interface to a zone.
 
 The zone can be either a string or an Entry object.
 */
-func (c *PanoZone) SetInterface(tmpl, ts, vsys string, zone interface{}, mode, iface string) error {
-	var name string
-
-	if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
+func (c *Panorama) SetInterface(tmpl, ts, vsys string, zone interface{}, mode, iface string) error {
+	names, err := toNames([]interface{}{zone})
+	if err != nil {
+		return err
 	}
+	name := names[0]
 
-	switch v := zone.(type) {
-	case string:
-		name = v
-	case Entry:
-		name = v.Name
+	switch mode {
+	case ModeL2, ModeL3, ModeVirtualWire, ModeTap, ModeExternal:
 	default:
-		return fmt.Errorf("Unknown type sent to %s set interface: %s", singular, v)
+		return fmt.Errorf("unknown mode value: %s", mode)
 	}
 
-	c.con.LogAction("(set) %s interface: %s", singular, name)
+	c.ns.Client.LogAction("(set) %s interface: %s", singular, name)
 
-	path := c.xpath(tmpl, ts, vsys, []string{name})
+	path, err := c.xpath(tmpl, ts, vsys, []string{name})
+	if err != nil {
+		return err
+	}
 	path = append(path, "network", mode)
 
-	_, err := c.con.Set(path, util.Member{Value: iface}, nil, nil)
+	_, err = c.ns.Client.Set(path, util.Member{Value: iface}, nil, nil)
 	return err
 }
 
@@ -54,159 +47,114 @@ DeleteInterface performs a DELETE to remove the interface from the zone.
 
 The zone can be either a string or an Entry object.
 */
-func (c *PanoZone) DeleteInterface(tmpl, ts, vsys string, zone interface{}, mode, iface string) error {
-	var name string
-
-	if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
+func (c *Panorama) DeleteInterface(tmpl, ts, vsys string, zone interface{}, mode, iface string) error {
+	names, err := toNames([]interface{}{zone})
+	if err != nil {
+		return err
 	}
+	name := names[0]
 
-	switch v := zone.(type) {
-	case string:
-		name = v
-	case Entry:
-		name = v.Name
+	switch mode {
+	case ModeL2, ModeL3, ModeVirtualWire, ModeTap, ModeExternal:
 	default:
-		return fmt.Errorf("Unknown type sent to %s delete interface: %s", singular, v)
+		return fmt.Errorf("unknown mode value: %s", mode)
 	}
 
-	c.con.LogAction("(delete) %s interface: %s", singular, name)
+	c.ns.Client.LogAction("(delete) %s interface: %s", singular, name)
 
-	path := c.xpath(tmpl, ts, vsys, []string{name})
+	path, err := c.xpath(tmpl, ts, vsys, []string{name})
+	if err != nil {
+		return err
+	}
 	path = append(path, "network", mode, util.AsMemberXpath([]string{iface}))
 
-	_, err := c.con.Delete(path, nil, nil)
+	_, err = c.ns.Client.Delete(path, nil, nil)
 	return err
 }
 
-// GetList performs GET to retrieve a list of values.
-func (c *PanoZone) GetList(tmpl, ts, vsys string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Get, c.xpath(tmpl, ts, vsys, nil), result)
+// GetList performs GET to retrieve a list of all objects.
+func (c *Panorama) GetList(tmpl, ts, vsys string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Get, c.pather(tmpl, ts, vsys), ans)
 }
 
-// ShowList performs SHOW to retrieve a list of values.
-func (c *PanoZone) ShowList(tmpl, ts, vsys string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Show, c.xpath(tmpl, ts, vsys, nil), result)
+// ShowList performs SHOW to retrieve a list of all objects.
+func (c *Panorama) ShowList(tmpl, ts, vsys string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Show, c.pather(tmpl, ts, vsys), ans)
 }
 
-// Get performs GET to retrieve information for the given uid.
-func (c *PanoZone) Get(tmpl, ts, vsys, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Get, c.xpath(tmpl, ts, vsys, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// Get performs GET to retrieve information for the given object.
+func (c *Panorama) Get(tmpl, ts, vsys, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Get, c.pather(tmpl, ts, vsys), name, ans)
+	return first(ans, err)
 }
 
-// GetAll performs GET to retrieve information for all objects.
-func (c *PanoZone) GetAll(tmpl, ts, vsys string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Get, c.xpath(tmpl, ts, vsys, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// Show performs SHOW to retrieve information for the given object.
+func (c *Panorama) Show(tmpl, ts, vsys, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Show, c.pather(tmpl, ts, vsys), name, ans)
+	return first(ans, err)
 }
 
-// Get performs SHOW to retrieve information for the given uid.
-func (c *PanoZone) Show(tmpl, ts, vsys, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Show, c.xpath(tmpl, ts, vsys, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// GetAll performs GET to retrieve all objects configured.
+func (c *Panorama) GetAll(tmpl, ts, vsys string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Get, c.pather(tmpl, ts, vsys), ans)
+	return all(ans, err)
 }
 
 // ShowAll performs SHOW to retrieve information for all objects.
-func (c *PanoZone) ShowAll(tmpl, ts, vsys string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Show, c.xpath(tmpl, ts, vsys, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+func (c *Panorama) ShowAll(tmpl, ts, vsys string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Show, c.pather(tmpl, ts, vsys), ans)
+	return all(ans, err)
 }
 
-// Set performs SET to create / update one or more objects.
-func (c *PanoZone) Set(tmpl, ts, vsys string, e ...Entry) error {
-	_, fn := c.versioning()
-	data := make([]interface{}, 0, len(e))
-	names := make([]string, 0, len(e))
-
-	for i := range e {
-		data = append(data, fn(e[i]))
-		names = append(names, e[i].Name)
-	}
-	path := c.xpath(tmpl, ts, vsys, names)
-
-	return c.ns.Set(names, path, data)
+// Set performs SET to configure the specified objects.
+func (c *Panorama) Set(tmpl, ts, vsys string, e ...Entry) error {
+	return c.ns.Set(c.pather(tmpl, ts, vsys), specifier(e...))
 }
 
-// Edit performs EDIT to create / update one object.
-func (c *PanoZone) Edit(tmpl, ts, vsys string, e Entry) error {
-	if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
-	}
-
-	_, fn := c.versioning()
-	path := c.xpath(tmpl, ts, vsys, []string{e.Name})
-	data := fn(e)
-
-	return c.ns.Edit(e.Name, path, data)
+// Edit performs EDIT to configure the specified object.
+func (c *Panorama) Edit(tmpl, ts, vsys string, e Entry) error {
+	return c.ns.Edit(c.pather(tmpl, ts, vsys), e)
 }
 
-// Delete removes the given objects.
+// Delete performs DELETE to remove the specified objects.
 //
 // Objects can be either a string or an Entry object.
-func (c *PanoZone) Delete(tmpl, ts, vsys string, e ...interface{}) error {
-	if len(e) == 0 {
-		return nil
-	} else if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
-	}
-
-	names := make([]string, len(e))
-	for i := range e {
-		switch v := e[i].(type) {
-		case string:
-			names[i] = v
-		case Entry:
-			names[i] = v.Name
-		default:
-			return fmt.Errorf("Unsupported type to delete: %s", v)
-		}
-	}
-
-	path := c.xpath(tmpl, ts, vsys, names)
-	return c.ns.Delete(names, path)
+func (c *Panorama) Delete(tmpl, ts, vsys string, e ...interface{}) error {
+	names, nErr := toNames(e)
+	return c.ns.Delete(c.pather(tmpl, ts, vsys), names, nErr)
 }
 
-/** Internal functions for this namespace struct **/
-
-func (c *PanoZone) versioning() (normalizer, func(Entry) interface{}) {
-	return &container_v1{}, specify_v1
+func (c *Panorama) pather(tmpl, ts, vsys string) namespace.Pather {
+	return func(v []string) ([]string, error) {
+		return c.xpath(tmpl, ts, vsys, v)
+	}
 }
 
-func (c *PanoZone) xpath(tmpl, ts, vsys string, vals []string) []string {
-	if vsys == "" {
-		vsys = "vsys1"
+func (c *Panorama) xpath(tmpl, ts, vsys string, vals []string) ([]string, error) {
+	if tmpl == "" && ts == "" {
+		return nil, fmt.Errorf("tmpl or ts must be specified")
+	}
+	if vsys == "shared" {
+		return nil, fmt.Errorf("vsys cannot be 'shared'")
 	}
 
 	ans := make([]string, 0, 12)
 	ans = append(ans, util.TemplateXpathPrefix(tmpl, ts)...)
+	ans = append(ans, util.VsysXpathPrefix(vsys)...)
 	ans = append(ans,
-		"config",
-		"devices",
-		util.AsEntryXpath([]string{"localhost.localdomain"}),
-		"vsys",
-		util.AsEntryXpath([]string{vsys}),
 		"zone",
 		util.AsEntryXpath(vals),
 	)
 
-	return ans
+	return ans, nil
+}
+
+func (c *Panorama) container() normalizer {
+	return container(c.ns.Client.Versioning())
 }
