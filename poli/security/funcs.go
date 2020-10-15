2fd7288b667db07626b684852688be42f0de834d
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-10-15 11:38:42 -0700

    reordering functions

--- poli/security/funcs.go
+++ poli/security/funcs.go
@@ -8,6 +8,10 @@ import (
 	"github.com/PaloAltoNetworks/pango/version"
 )
 
+func versioning(v version.Number) (normalizer, func(Entry) interface{}) {
+	return &container_v1{}, specify_v1
+}
+
 func specifier(e ...Entry) []namespace.Specifier {
 	ans := make([]namespace.Specifier, 0, len(e))
 
@@ -18,10 +22,6 @@ func specifier(e ...Entry) []namespace.Specifier {
 	}
 
 	return ans
-}
-
-func versioning(v version.Number) (normalizer, func(Entry) interface{}) {
-	return &container_v1{}, specify_v1
 }
 
 func container(v version.Number) normalizer {
