0d5953c8f430c48f167906997c0e74b8ea870f82
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-10-15 08:50:51 -0700

    namespace update: tunnel interfaces

--- netw/interface/tunnel/pano.go
+++ netw/interface/tunnel/pano.go
@@ -1,204 +1,86 @@
 package tunnel
 
 import (
-	"encoding/xml"
 	"fmt"
 
 	"github.com/PaloAltoNetworks/pango/namespace"
 	"github.com/PaloAltoNetworks/pango/util"
 )
 
-// PanoTunnel is the client.Network.TunnelInterface namespace.
-type PanoTunnel struct {
-	con util.XapiClient
-	ns  *namespace.Namespace
+// Panorama is the client.Network.TunnelInterface namespace.
+type Panorama struct {
+	ns *namespace.Importable
 }
 
-// Initialize is invoked by client.Initialize().
-func (c *PanoTunnel) Initialize(con util.XapiClient) {
-	c.con = con
-	c.ns = namespace.New(singular, plural, con)
+// GetList performs GET to retrieve a list of all objects.
+func (c *Panorama) GetList(tmpl, ts string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Get, c.pather(tmpl, ts), ans)
 }
 
-// ShowList performs SHOW to retrieve a list of tunnel interfaces.
-func (c *PanoTunnel) ShowList(tmpl, ts string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Show, c.xpath(tmpl, ts, nil), result)
+// ShowList performs a SHOW to retrieve a list of all objects.
+func (c *Panorama) ShowList(tmpl, ts string) ([]string, error) {
+	ans := c.container()
+	return c.ns.Listing(util.Show, c.pather(tmpl, ts), ans)
 }
 
-// GetList performs GET to retrieve a list of tunnel interfaces.
-func (c *PanoTunnel) GetList(tmpl, ts string) ([]string, error) {
-	result, _ := c.versioning()
-	return c.ns.Listing(util.Get, c.xpath(tmpl, ts, nil), result)
+// Get performs GET to retrieve configuration for the given object.
+func (c *Panorama) Get(tmpl, ts, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Get, c.pather(tmpl, ts), name, ans)
+	return first(ans, err)
 }
 
-// Get performs GET to retrieve information for the given tunnel interface.
-func (c *PanoTunnel) Get(tmpl, ts, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Get, c.xpath(tmpl, ts, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// Show performs SHOW to retrieve configuration for the given object.
+func (c *Panorama) Show(tmpl, ts, name string) (Entry, error) {
+	ans := c.container()
+	err := c.ns.Object(util.Show, c.pather(tmpl, ts), name, ans)
+	return first(ans, err)
 }
 
-// GetAll performs GET to retrieve information for all objects.
-func (c *PanoTunnel) GetAll(tmpl, ts string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Get, c.xpath(tmpl, ts, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// GetAll performs GET to retrieve all objects configured.
+func (c *Panorama) GetAll(tmpl, ts string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Get, c.pather(tmpl, ts), ans)
+	return all(ans, err)
 }
 
-// Show performs SHOW to retrieve information for the given tunnel interface.
-func (c *PanoTunnel) Show(tmpl, ts, name string) (Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Object(util.Show, c.xpath(tmpl, ts, []string{name}), name, result); err != nil {
-		return Entry{}, err
-	}
-
-	return result.Normalize()[0], nil
+// ShowAll performs SHOW to retrieve all objects configured.
+func (c *Panorama) ShowAll(tmpl, ts string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.Objects(util.Show, c.pather(tmpl, ts), ans)
+	return all(ans, err)
 }
 
-// ShowAll performs SHOW to retrieve information for all objects.
-func (c *PanoTunnel) ShowAll(tmpl, ts string) ([]Entry, error) {
-	result, _ := c.versioning()
-	if err := c.ns.Objects(util.Show, c.xpath(tmpl, ts, nil), result); err != nil {
-		return nil, err
-	}
-
-	return result.Normalize(), nil
+// Set performs SET to configure the specified objects.
+func (c *Panorama) Set(tmpl, ts, vsys string, e ...Entry) error {
+	return c.ns.Set(tmpl, ts, vsys, c.pather(tmpl, ts), specifier(e...))
 }
 
-// Set performs SET to create / update one or more tunnel interfaces.
+// Edit performs EDIT to configure the specified object.
+func (c *Panorama) Edit(tmpl, ts, vsys string, e Entry) error {
+	return c.ns.Edit(tmpl, ts, vsys, c.pather(tmpl, ts), e)
+}
+
+// Delete performs DELETE to remove the specified objects.
 //
-// Specifying a non-empty vsys will import the interfaces into that vsys,
-// allowing the vsys to use them.
-func (c *PanoTunnel) Set(tmpl, ts, vsys string, e ...Entry) error {
-	var err error
-
-	if len(e) == 0 {
-		return nil
-	} else if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
-	} else if vsys == "" {
-		return fmt.Errorf("vsys must be specified")
-	}
-
-	_, fn := c.versioning()
-	names := make([]string, len(e))
-
-	// Build up the struct with the given interface configs.
-	d := util.BulkElement{XMLName: xml.Name{Local: "units"}}
-	for i := range e {
-		d.Data = append(d.Data, fn(e[i]))
-		names[i] = e[i].Name
-	}
-	c.con.LogAction("(set) %s: %v", plural, names)
-
-	// Set xpath.
-	path := c.xpath(tmpl, ts, names)
-	if len(e) == 1 {
-		path = path[:len(path)-1]
-	} else {
-		path = path[:len(path)-2]
-	}
-
-	// Create the interfaces.
-	_, err = c.con.Set(path, d.Config(), nil, nil)
-	if err != nil {
-		return err
-	}
-
-	// Remove the interfaces from any vsys they're currently in.
-	if err = c.con.VsysUnimport(util.InterfaceImport, tmpl, ts, names); err != nil {
-		return err
-	}
-
-	// Perform vsys import next.
-	return c.con.VsysImport(util.InterfaceImport, tmpl, ts, vsys, names)
+// Objects can be either a string or an Entry object.
+func (c *Panorama) Delete(tmpl, ts string, e ...interface{}) error {
+	names, nErr := toNames(e)
+	return c.ns.Delete(tmpl, ts, c.pather(tmpl, ts), names, nErr)
 }
 
-// Edit performs EDIT to create / update the specified tunnel interface.
-//
-// Specifying a non-empty vsys will import the interface into that vsys,
-// allowing the vsys to use it.
-func (c *PanoTunnel) Edit(tmpl, ts, vsys string, e Entry) error {
-	var err error
+func (c *Panorama) pather(tmpl, ts string) namespace.Pather {
+	return func(v []string) ([]string, error) {
+		return c.xpath(tmpl, ts, v)
+	}
+}
 
+func (c *Panorama) xpath(tmpl, ts string, vals []string) ([]string, error) {
 	if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
-	} else if vsys == "" {
-		return fmt.Errorf("vsys must be specified")
+		return nil, fmt.Errorf("tmpl or ts must be specified")
 	}
 
-	_, fn := c.versioning()
-
-	c.con.LogAction("(edit) %s: %q", singular, e.Name)
-
-	// Set xpath.
-	path := c.xpath(tmpl, ts, []string{e.Name})
-
-	// Edit the interface.
-	_, err = c.con.Edit(path, fn(e), nil, nil)
-	if err != nil {
-		return err
-	}
-
-	// Remove the interface from any vsys it's currently in.
-	if err = c.con.VsysUnimport(util.InterfaceImport, tmpl, ts, []string{e.Name}); err != nil {
-		return err
-	}
-
-	// Import the interface.
-	return c.con.VsysImport(util.InterfaceImport, tmpl, ts, vsys, []string{e.Name})
-}
-
-// Delete removes the given tunnel interface(s) from the firewall.
-//
-// Interfaces can be a string or an Entry object.
-func (c *PanoTunnel) Delete(tmpl, ts string, e ...interface{}) error {
-	var err error
-
-	if len(e) == 0 {
-		return nil
-	} else if tmpl == "" && ts == "" {
-		return fmt.Errorf("tmpl or ts must be specified")
-	}
-
-	names := make([]string, len(e))
-	for i := range e {
-		switch v := e[i].(type) {
-		case string:
-			names[i] = v
-		case Entry:
-			names[i] = v.Name
-		default:
-			return fmt.Errorf("Unknown type sent to delete: %s", v)
-		}
-	}
-	c.con.LogAction("(delete) %s: %v", plural, names)
-
-	// Unimport interfaces.
-	if err = c.con.VsysUnimport(util.InterfaceImport, tmpl, ts, names); err != nil {
-		return err
-	}
-
-	// Remove interfaces next.
-	path := c.xpath(tmpl, ts, names)
-	_, err = c.con.Delete(path, nil, nil)
-	return err
-}
-
-/** Internal functions for this namespace struct **/
-
-func (c *PanoTunnel) versioning() (normalizer, func(Entry) interface{}) {
-	return &container_v1{}, specify_v1
-}
-
-func (c *PanoTunnel) xpath(tmpl, ts string, vals []string) []string {
 	ans := make([]string, 0, 13)
 	ans = append(ans, util.TemplateXpathPrefix(tmpl, ts)...)
 	ans = append(ans,
@@ -212,5 +94,9 @@ func (c *PanoTunnel) xpath(tmpl, ts string, vals []string) []string {
 		util.AsEntryXpath(vals),
 	)
 
-	return ans
+	return ans, nil
+}
+
+func (c *Panorama) container() normalizer {
+	return container(c.ns.Client.Versioning())
 }
