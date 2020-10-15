af79c3464035745bc0e9123a280b144c87bf9f9c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-10-15 15:19:54 -0700

    feat: Add Dynamic user group support

---
+++ objs/dug/pano_test.go
@@ -0,0 +1,35 @@
+package dug
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/PaloAltoNetworks/pango/testdata"
+)
+
+func TestPanoNormalization(t *testing.T) {
+	testCases := getTests()
+
+	mc := &testdata.MockClient{}
+	ns := PanoramaNamespace(mc)
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			mc.Version = tc.version
+			mc.Reset()
+			mc.AddResp("")
+			err := ns.Set(tc.dg, tc.conf)
+			if err != nil {
+				t.Errorf("Error in set: %s", err)
+			} else {
+				mc.AddResp(mc.Elm)
+				r, err := ns.Get(tc.dg, tc.conf.Name)
+				if err != nil {
+					t.Errorf("Error in get: %s", err)
+				} else if !reflect.DeepEqual(tc.conf, r) {
+					t.Errorf("%#v != %#v", tc.conf, r)
+				}
+			}
+		})
+	}
+}
---
+++ objs/dug/testdata_test.go
@@ -0,0 +1,29 @@
+package dug
+
+import (
+	"github.com/PaloAltoNetworks/pango/version"
+)
+
+type testCase struct {
+	desc    string
+	vsys    string
+	dg      string
+	version version.Number
+	conf    Entry
+}
+
+func getTests() []testCase {
+	return []testCase{
+		{"v1 single with no tags", "", "", version.Number{9, 1, 0, ""}, Entry{
+			Name:        "one",
+			Description: "my description",
+            Filter: "'single'",
+		}},
+		{"v1 single with tags", "", "", version.Number{9, 1, 0, ""}, Entry{
+			Name:        "two",
+			Description: "my second description",
+            Filter: "'single' and 'foo'",
+            Tags: []string{"tag2", "tag1"},
+		}},
+	}
+}
