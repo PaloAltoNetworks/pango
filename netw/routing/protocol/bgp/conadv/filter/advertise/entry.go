9cd0a451cabcd9eb78121711badbb8f9decc6674
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-12-11 14:52:05 -0800

    work on #29 - update bgp conadv advertise filter namespace

--- netw/routing/protocol/bgp/conadv/filter/advertise/entry.go
+++ netw/routing/protocol/bgp/conadv/filter/advertise/entry.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 
 	"github.com/PaloAltoNetworks/pango/util"
+	"github.com/PaloAltoNetworks/pango/version"
 )
 
 // Entry is a normalized, version independent representation of a BGP
@@ -37,64 +38,106 @@ func (o *Entry) Copy(s Entry) {
 
 /** Structs / functions for this namespace. **/
 
+func (o Entry) Specify(v version.Number) (string, interface{}) {
+	_, fn := versioning(v)
+	return o.Name, fn(o)
+}
+
 type normalizer interface {
-	Normalize() Entry
+	Normalize() []Entry
+	Names() []string
 }
 
 type container_v1 struct {
-	Answer entry_v1 `xml:"result>entry"`
+	Answer []entry_v1 `xml:"entry"`
 }
 
-func (o *container_v1) Normalize() Entry {
+func (o *container_v1) Normalize() []Entry {
+	ans := make([]Entry, 0, len(o.Answer))
+	for i := range o.Answer {
+		ans = append(ans, o.Answer[i].normalize())
+	}
+
+	return ans
+}
+
+func (o *container_v1) Names() []string {
+	ans := make([]string, 0, len(o.Answer))
+	for i := range o.Answer {
+		ans = append(ans, o.Answer[i].Name)
+	}
+
+	return ans
+}
+
+func (o *entry_v1) normalize() Entry {
 	ans := Entry{
-		Name:          o.Answer.Name,
-		Enable:        util.AsBool(o.Answer.Enable),
-		Med:           o.Answer.Med,
-		AddressPrefix: util.EntToStr(o.Answer.AddressPrefix),
-		NextHop:       util.MemToStr(o.Answer.NextHop),
-		FromPeer:      util.MemToStr(o.Answer.FromPeer),
+		Name:          o.Name,
+		Enable:        util.AsBool(o.Enable),
+		Med:           o.Med,
+		AddressPrefix: util.EntToStr(o.AddressPrefix),
+		NextHop:       util.MemToStr(o.NextHop),
+		FromPeer:      util.MemToStr(o.FromPeer),
 	}
 
-	if o.Answer.AsPathRegex != nil {
-		ans.AsPathRegex = o.Answer.AsPathRegex.Regex
+	if o.AsPathRegex != nil {
+		ans.AsPathRegex = o.AsPathRegex.Regex
 	}
 
-	if o.Answer.CommunityRegex != nil {
-		ans.CommunityRegex = o.Answer.CommunityRegex.Regex
+	if o.CommunityRegex != nil {
+		ans.CommunityRegex = o.CommunityRegex.Regex
 	}
 
-	if o.Answer.ExtendedCommunityRegex != nil {
-		ans.ExtendedCommunityRegex = o.Answer.ExtendedCommunityRegex.Regex
+	if o.ExtendedCommunityRegex != nil {
+		ans.ExtendedCommunityRegex = o.ExtendedCommunityRegex.Regex
 	}
 
 	return ans
 }
 
 type container_v2 struct {
-	Answer entry_v2 `xml:"result>entry"`
+	Answer []entry_v2 `xml:"entry"`
 }
 
-func (o *container_v2) Normalize() Entry {
+func (o *container_v2) Normalize() []Entry {
+	ans := make([]Entry, 0, len(o.Answer))
+	for i := range o.Answer {
+		ans = append(ans, o.Answer[i].normalize())
+	}
+
+	return ans
+}
+
+func (o *container_v2) Names() []string {
+	ans := make([]string, 0, len(o.Answer))
+	for i := range o.Answer {
+		ans = append(ans, o.Answer[i].Name)
+	}
+
+	return ans
+}
+
+func (o *entry_v2) normalize() Entry {
 	ans := Entry{
-		Name:          o.Answer.Name,
-		Enable:        util.AsBool(o.Answer.Enable),
-		Med:           o.Answer.Med,
-		RouteTable:    o.Answer.RouteTable,
-		AddressPrefix: util.EntToStr(o.Answer.AddressPrefix),
-		NextHop:       util.MemToStr(o.Answer.NextHop),
-		FromPeer:      util.MemToStr(o.Answer.FromPeer),
+		Name:          o.Name,
+		Enable:        util.AsBool(o.Enable),
+		Med:           o.Med,
+		RouteTable:    o.RouteTable,
+		AddressPrefix: util.EntToStr(o.AddressPrefix),
+		NextHop:       util.MemToStr(o.NextHop),
+		FromPeer:      util.MemToStr(o.FromPeer),
 	}
 
-	if o.Answer.AsPathRegex != nil {
-		ans.AsPathRegex = o.Answer.AsPathRegex.Regex
+	if o.AsPathRegex != nil {
+		ans.AsPathRegex = o.AsPathRegex.Regex
 	}
 
-	if o.Answer.CommunityRegex != nil {
-		ans.CommunityRegex = o.Answer.CommunityRegex.Regex
+	if o.CommunityRegex != nil {
+		ans.CommunityRegex = o.CommunityRegex.Regex
 	}
 
-	if o.Answer.ExtendedCommunityRegex != nil {
-		ans.ExtendedCommunityRegex = o.Answer.ExtendedCommunityRegex.Regex
+	if o.ExtendedCommunityRegex != nil {
+		ans.ExtendedCommunityRegex = o.ExtendedCommunityRegex.Regex
 	}
 
 	return ans
