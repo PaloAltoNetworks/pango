b2dad1d34430983a7dd577d62946d2a9aac2adb0
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-12-11 11:54:38 -0800

    work on #29 - update agg suppress filters

--- netw/routing/protocol/bgp/aggregate/filter/suppress/fw_test.go
+++ netw/routing/protocol/bgp/aggregate/filter/suppress/fw_test.go
@@ -5,61 +5,13 @@ import (
 	"testing"
 
 	"github.com/PaloAltoNetworks/pango/testdata"
-	"github.com/PaloAltoNetworks/pango/version"
 )
 
 func TestFwNormalization(t *testing.T) {
-	testCases := []struct {
-		desc    string
-		version version.Number
-		conf    Entry
-	}{
-		{"v1 minimal", version.Number{7, 0, 0, ""}, Entry{
-			Name:   "minimal",
-			Enable: false,
-		}},
-		{"v1 full", version.Number{7, 0, 0, ""}, Entry{
-			Name:                   "foo",
-			Enable:                 true,
-			AsPathRegex:            "as path regex",
-			CommunityRegex:         "community regex",
-			ExtendedCommunityRegex: "extended community regex",
-			Med:                    "match med",
-			AddressPrefix: map[string]bool{
-				"5.5.0.0": true,
-				"6.6.0.0": false,
-				"7.7.0.0": true,
-				"8.8.0.0": false,
-			},
-			NextHop:  []string{"nh1", "nh2"},
-			FromPeer: []string{"fp1", "fp2"},
-		}},
-		{"v2 minimal", version.Number{8, 0, 0, ""}, Entry{
-			Name:   "minimal",
-			Enable: false,
-		}},
-		{"v2 full", version.Number{8, 0, 0, ""}, Entry{
-			Name:                   "foo",
-			Enable:                 true,
-			AsPathRegex:            "as path regex",
-			CommunityRegex:         "community regex",
-			ExtendedCommunityRegex: "extended community regex",
-			Med:                    "match med",
-			RouteTable:             RouteTableBoth,
-			AddressPrefix: map[string]bool{
-				"5.5.0.0": true,
-				"6.6.0.0": false,
-				"7.7.0.0": true,
-				"8.8.0.0": false,
-			},
-			NextHop:  []string{"nh1", "nh2"},
-			FromPeer: []string{"fp1", "fp2"},
-		}},
-	}
+	testCases := getTests()
 
 	mc := &testdata.MockClient{}
-	ns := &FwSuppress{}
-	ns.Initialize(mc)
+	ns := FirewallNamespace(mc)
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
---
+++ netw/routing/protocol/bgp/aggregate/filter/suppress/testdata_test.go
@@ -0,0 +1,57 @@
+package suppress
+
+import (
+	"github.com/PaloAltoNetworks/pango/version"
+)
+
+type testCase struct {
+	desc    string
+	version version.Number
+	conf    Entry
+}
+
+func getTests() []testCase {
+	return []testCase{
+		{"v1 minimal", version.Number{7, 0, 0, ""}, Entry{
+			Name:   "minimal",
+			Enable: false,
+		}},
+		{"v1 full", version.Number{7, 0, 0, ""}, Entry{
+			Name:                   "foo",
+			Enable:                 true,
+			AsPathRegex:            "as path regex",
+			CommunityRegex:         "community regex",
+			ExtendedCommunityRegex: "extended community regex",
+			Med:                    "match med",
+			AddressPrefix: map[string]bool{
+				"5.5.0.0": true,
+				"6.6.0.0": false,
+				"7.7.0.0": true,
+				"8.8.0.0": false,
+			},
+			NextHop:  []string{"nh1", "nh2"},
+			FromPeer: []string{"fp1", "fp2"},
+		}},
+		{"v2 minimal", version.Number{8, 0, 0, ""}, Entry{
+			Name:   "minimal",
+			Enable: false,
+		}},
+		{"v2 full", version.Number{8, 0, 0, ""}, Entry{
+			Name:                   "foo",
+			Enable:                 true,
+			AsPathRegex:            "as path regex",
+			CommunityRegex:         "community regex",
+			ExtendedCommunityRegex: "extended community regex",
+			Med:                    "match med",
+			RouteTable:             RouteTableBoth,
+			AddressPrefix: map[string]bool{
+				"5.5.0.0": true,
+				"6.6.0.0": false,
+				"7.7.0.0": true,
+				"8.8.0.0": false,
+			},
+			NextHop:  []string{"nh1", "nh2"},
+			FromPeer: []string{"fp1", "fp2"},
+		}},
+	}
+}
