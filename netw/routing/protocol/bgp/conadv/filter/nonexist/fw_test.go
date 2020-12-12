95eb598855a9093defc060c94d79f9e7a2089b7b
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-12-11 16:29:57 -0800

    work on #29 - update conadv nonexist filter

--- netw/routing/protocol/bgp/conadv/filter/nonexist/fw_test.go
+++ netw/routing/protocol/bgp/conadv/filter/nonexist/fw_test.go
@@ -5,51 +5,13 @@ import (
 	"testing"
 
 	"github.com/PaloAltoNetworks/pango/testdata"
-	"github.com/PaloAltoNetworks/pango/version"
 )
 
 func TestFwNormalization(t *testing.T) {
-	testCases := []struct {
-		desc    string
-		version version.Number
-		conf    Entry
-	}{
-		{"v1 basic", version.Number{7, 0, 0, ""}, Entry{
-			Name:   "one",
-			Enable: false,
-		}},
-		{"v1 full", version.Number{7, 0, 0, ""}, Entry{
-			Name:                   "two",
-			Enable:                 true,
-			AsPathRegex:            "as path regex",
-			CommunityRegex:         "community regex",
-			ExtendedCommunityRegex: "extended community regex",
-			Med:                    "match med",
-			AddressPrefix:          []string{"5.5.0.0", "6.6.0.0"},
-			NextHop:                []string{"nh1", "nh2"},
-			FromPeer:               []string{"fp1", "fp2"},
-		}},
-		{"v2 basic", version.Number{8, 0, 0, ""}, Entry{
-			Name:   "three",
-			Enable: false,
-		}},
-		{"v2 full", version.Number{8, 0, 0, ""}, Entry{
-			Name:                   "four",
-			Enable:                 true,
-			AsPathRegex:            "as path regex",
-			CommunityRegex:         "community regex",
-			ExtendedCommunityRegex: "extended community regex",
-			Med:                    "match med",
-			RouteTable:             RouteTableUnicast,
-			AddressPrefix:          []string{"5.5.0.0", "6.6.0.0"},
-			NextHop:                []string{"nh1", "nh2"},
-			FromPeer:               []string{"fp1", "fp2"},
-		}},
-	}
+	testCases := getTests()
 
 	mc := &testdata.MockClient{}
-	ns := &FwNonExist{}
-	ns.Initialize(mc)
+	ns := FirewallNamespace(mc)
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
---
+++ netw/routing/protocol/bgp/conadv/filter/nonexist/testdata_test.go
@@ -0,0 +1,47 @@
+package nonexist
+
+import (
+	"github.com/PaloAltoNetworks/pango/version"
+)
+
+type testCase struct {
+	desc    string
+	version version.Number
+	conf    Entry
+}
+
+func getTests() []testCase {
+	return []testCase{
+		{"v1 basic", version.Number{7, 0, 0, ""}, Entry{
+			Name:   "one",
+			Enable: false,
+		}},
+		{"v1 full", version.Number{7, 0, 0, ""}, Entry{
+			Name:                   "two",
+			Enable:                 true,
+			AsPathRegex:            "as path regex",
+			CommunityRegex:         "community regex",
+			ExtendedCommunityRegex: "extended community regex",
+			Med:                    "match med",
+			AddressPrefix:          []string{"5.5.0.0", "6.6.0.0"},
+			NextHop:                []string{"nh1", "nh2"},
+			FromPeer:               []string{"fp1", "fp2"},
+		}},
+		{"v2 basic", version.Number{8, 0, 0, ""}, Entry{
+			Name:   "three",
+			Enable: false,
+		}},
+		{"v2 full", version.Number{8, 0, 0, ""}, Entry{
+			Name:                   "four",
+			Enable:                 true,
+			AsPathRegex:            "as path regex",
+			CommunityRegex:         "community regex",
+			ExtendedCommunityRegex: "extended community regex",
+			Med:                    "match med",
+			RouteTable:             RouteTableUnicast,
+			AddressPrefix:          []string{"5.5.0.0", "6.6.0.0"},
+			NextHop:                []string{"nh1", "nh2"},
+			FromPeer:               []string{"fp1", "fp2"},
+		}},
+	}
+}
