44eff7787acf6dcd9e1f0dfc333bf1383c40a6a5
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-01-15 09:25:09 -0800

    updating file blocking tests

--- objs/profile/security/file/testdata_test.go
+++ objs/profile/security/file/testdata_test.go
@@ -57,36 +57,5 @@ func getTests() []tc {
 				},
 			},
 		}},
-		{"v2 no rules", version.Number{7, 0, 0, ""}, Entry{
-			Name:        "t1",
-			Description: "foobar",
-		}},
-		{"v1 with rules", version.Number{7, 0, 0, ""}, Entry{
-			Name:        "t2",
-			Description: "second",
-			Rules: []Rule{
-				Rule{
-					Name:         "rule1",
-					Applications: []string{"app1", "app2"},
-					FileTypes:    []string{"7z"},
-					Direction:    DirectionDownload,
-					Action:       ActionAlert,
-				},
-				Rule{
-					Name:         "rule2",
-					Applications: []string{"app1"},
-					FileTypes:    []string{"mpeg2", "mpeg3", "mpeg4"},
-					Direction:    DirectionUpload,
-					Action:       ActionBlock,
-				},
-				Rule{
-					Name:         "rule3",
-					Applications: []string{"app3"},
-					FileTypes:    []string{"mp3", "ogg"},
-					Direction:    DirectionBoth,
-					Action:       ActionContinue,
-				},
-			},
-		}},
 	}
 }
