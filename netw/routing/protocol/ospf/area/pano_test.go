b113d917dc777f1ecfc3a7c18c29544bd1e1891d
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-01-27 22:00:57 -0800

    adding ospf areas to panorama

---
+++ netw/routing/protocol/ospf/area/pano_test.go
@@ -0,0 +1,37 @@
+package area
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/PaloAltoNetworks/pango/testdata"
+)
+
+func TestPanoNormalization(t *testing.T) {
+	testCases := getTests()
+
+	mc := &testdata.MockClient{}
+	ns := PanoramaNamespace(mc)
+
+	vr := "mockVr"
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			mc.Version = tc.version
+			mc.Reset()
+			mc.AddResp("")
+			err := ns.Set("my template", "", vr, tc.conf)
+			if err != nil {
+				t.Errorf("Error in set: %s", err)
+			} else {
+				mc.AddResp(mc.Elm)
+				r, err := ns.Get("my template", "", vr, tc.conf.Name)
+				if err != nil {
+					t.Errorf("Error in get: %s", err)
+				}
+				if !reflect.DeepEqual(tc.conf, r) {
+					t.Errorf("%#v != %#v", tc.conf, r)
+				}
+			}
+		})
+	}
+}
