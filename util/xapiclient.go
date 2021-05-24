af80af1287edbf288b1a1f7ed11c8d5000147b1a
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-05-24 15:32:58 -0700

    add new functions to the test struct

--- util/xapiclient.go
+++ util/xapiclient.go
@@ -14,6 +14,7 @@ type XapiClient interface {
 	LogQuery(string, ...interface{})
 	LogOp(string, ...interface{})
 	LogUid(string, ...interface{})
+	LogLog(string, ...interface{})
 	Op(interface{}, string, interface{}, interface{}) ([]byte, error)
 	Show(interface{}, interface{}, interface{}) ([]byte, error)
 	Get(interface{}, interface{}, interface{}) ([]byte, error)
@@ -21,6 +22,7 @@ type XapiClient interface {
 	Set(interface{}, interface{}, interface{}, interface{}) ([]byte, error)
 	Edit(interface{}, interface{}, interface{}, interface{}) ([]byte, error)
 	Move(interface{}, string, string, interface{}, interface{}) ([]byte, error)
+	Log(string, string, string, string, int, int, interface{}, interface{}) ([]byte, error)
 	Uid(interface{}, string, interface{}, interface{}) ([]byte, error)
 	EntryListUsing(Retriever, []string) ([]string, error)
 	MemberListUsing(Retriever, []string) ([]string, error)
@@ -28,6 +30,8 @@ type XapiClient interface {
 	VsysImport(string, string, string, string, []string) error
 	VsysUnimport(string, string, string, []string) error
 	WaitForJob(uint, time.Duration, interface{}) error
+	WaitForLogs(uint, time.Duration, time.Duration, interface{}) ([]byte, error)
 	Commit(interface{}, string, interface{}) (uint, []byte, error)
+	Clock() (time.Time, error)
 	PositionFirstEntity(int, string, string, []string, []string) error
 }
