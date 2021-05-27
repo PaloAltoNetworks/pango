46c438ac81e49f1b277ba992ed2e3f2590c7b961
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-05-27 09:43:44 -0700

    work on #29 - templates

--- pnrm/template/pano_test.go
+++ pnrm/template/pano_test.go
@@ -69,8 +69,7 @@ func TestNormalization(t *testing.T) {
 	}
 
 	mc := &testdata.MockClient{}
-	ns := &Template{}
-	ns.Initialize(mc)
+	ns := PanoramaNamespace(mc)
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
