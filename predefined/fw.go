448bc7ce16512501ec6eaf04e6ee10194400a4cd
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-06-03 14:01:04 -0700

    make predefined service retrieval available

--- predefined/fw.go
+++ predefined/fw.go
@@ -2,6 +2,7 @@ package predefined
 
 import (
 	"github.com/PaloAltoNetworks/pango/objs/app"
+	"github.com/PaloAltoNetworks/pango/objs/srvc"
 	dlpft "github.com/PaloAltoNetworks/pango/predefined/dlp/filetype"
 	tdbft "github.com/PaloAltoNetworks/pango/predefined/tdb/filetype"
 	"github.com/PaloAltoNetworks/pango/predefined/threat"
@@ -11,6 +12,7 @@ import (
 type Firewall struct {
 	Application *app.Predefined
 	DlpFileType *dlpft.Firewall
+	Services    *srvc.Predefined
 	TdbFileType *tdbft.Firewall
 	Threat      *threat.Firewall
 }
@@ -19,6 +21,7 @@ func FirewallNamespace(x util.XapiClient) *Firewall {
 	return &Firewall{
 		Application: app.PredefinedNamespace(x),
 		DlpFileType: dlpft.FirewallNamespace(x),
+		Services:    srvc.PredefinedNamespace(x),
 		TdbFileType: tdbft.FirewallNamespace(x),
 		Threat:      threat.FirewallNamespace(x),
 	}
--- predefined/pano.go
+++ predefined/pano.go
@@ -2,6 +2,7 @@ package predefined
 
 import (
 	"github.com/PaloAltoNetworks/pango/objs/app"
+	"github.com/PaloAltoNetworks/pango/objs/srvc"
 	dlpft "github.com/PaloAltoNetworks/pango/predefined/dlp/filetype"
 	tdbft "github.com/PaloAltoNetworks/pango/predefined/tdb/filetype"
 	"github.com/PaloAltoNetworks/pango/predefined/threat"
@@ -11,6 +12,7 @@ import (
 type Panorama struct {
 	Application *app.Predefined
 	DlpFileType *dlpft.Panorama
+	Services    *srvc.Predefined
 	TdbFileType *tdbft.Panorama
 	Threat      *threat.Panorama
 }
@@ -19,6 +21,7 @@ func PanoramaNamespace(x util.XapiClient) *Panorama {
 	return &Panorama{
 		Application: app.PredefinedNamespace(x),
 		DlpFileType: dlpft.PanoramaNamespace(x),
+		Services:    srvc.PredefinedNamespace(x),
 		TdbFileType: tdbft.PanoramaNamespace(x),
 		Threat:      threat.PanoramaNamespace(x),
 	}
