8ac6e4e632f6d7190ce343980f33537b64aebe36
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-07-08 09:39:52 -0700

    add uuid (9.0) and source/dest devices to security rules (10.0)

--- poli/security/testdata_test.go
+++ poli/security/testdata_test.go
@@ -32,5 +32,50 @@ func getTests() []testCase {
 			},
 			NegateTarget: true,
 		}},
+		{version.Number{9, 0, 0, ""}, "basic rule", "", "", true, Entry{
+			Name: "rule1",
+		}},
+		{version.Number{9, 0, 0, ""}, "rule with uuid", "", "", true, Entry{
+			Name: "rule1",
+			Uuid: "123-456-78",
+		}},
+		{version.Number{9, 0, 0, ""}, "prerulebase rule", "vsys2", util.PreRulebase, true, Entry{
+			Name:     "rule2",
+			LogEnd:   true,
+			Disabled: true,
+		}},
+		{version.Number{9, 0, 0, ""}, "postrulebase rule with target", "vsys3", util.PostRulebase, true, Entry{
+			Name: "rule3",
+			Targets: map[string][]string{
+				"fw1": nil,
+				"fw2": {"vsys2", "vsys3"},
+			},
+			NegateTarget: true,
+		}},
+		{version.Number{10, 0, 0, ""}, "basic rule", "", "", true, Entry{
+			Name: "rule1",
+		}},
+		{version.Number{10, 0, 0, ""}, "rule with uuid", "", "", true, Entry{
+			Name: "rule1",
+			Uuid: "123-456-78",
+		}},
+		{version.Number{10, 0, 0, ""}, "rule with source and dest devices", "", "", true, Entry{
+			Name:               "rule1",
+			SourceDevices:      []string{"src1", "wu tang"},
+			DestinationDevices: []string{"dest1", "clan"},
+		}},
+		{version.Number{10, 0, 0, ""}, "prerulebase rule", "vsys2", util.PreRulebase, true, Entry{
+			Name:     "rule2",
+			LogEnd:   true,
+			Disabled: true,
+		}},
+		{version.Number{10, 0, 0, ""}, "postrulebase rule with target", "vsys3", util.PostRulebase, true, Entry{
+			Name: "rule3",
+			Targets: map[string][]string{
+				"fw1": nil,
+				"fw2": {"vsys2", "vsys3"},
+			},
+			NegateTarget: true,
+		}},
 	}
 }
