1fea282983d5ea515173ad21dbcc779e4645d1bc
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-01 20:57:57 -0700

    add client.Network.BgpAggregation config load

--- netw/routing/protocol/bgp/aggregate/fw.go
+++ netw/routing/protocol/bgp/aggregate/fw.go
@@ -70,6 +70,13 @@ func (c *Firewall) Delete(vr string, e ...interface{}) error {
 	return c.ns.Delete(c.pather(vr), names, nErr)
 }
 
+// FromPanosConfig retrieves all objects stored in the retrieved config.
+func (c *Firewall) FromPanosConfig(vr string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.FromPanosConfig(c.pather(vr), ans)
+	return all(ans, err)
+}
+
 func (c *Firewall) pather(vr string) namespace.Pather {
 	return func(v []string) ([]string, error) {
 		return c.xpath(vr, v)
