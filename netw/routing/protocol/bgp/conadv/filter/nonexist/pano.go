f4be14eae618565a7cdb4495560e6a767ea54f47
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-01 20:58:44 -0700

    add client.Network.BgpConAdvNonExistFilter config load

--- netw/routing/protocol/bgp/conadv/filter/nonexist/pano.go
+++ netw/routing/protocol/bgp/conadv/filter/nonexist/pano.go
@@ -70,6 +70,13 @@ func (c *Panorama) Delete(tmpl, ts, vr, ca string, e ...interface{}) error {
 	return c.ns.Delete(c.pather(tmpl, ts, vr, ca), names, nErr)
 }
 
+// FromPanosConfig retrieves all objects stored in the retrieved config.
+func (c *Panorama) FromPanosConfig(tmpl, ts, vr, ca string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.FromPanosConfig(c.pather(tmpl, ts, vr, ca), ans)
+	return all(ans, err)
+}
+
 func (c *Panorama) pather(tmpl, ts, vr, ca string) namespace.Pather {
 	return func(v []string) ([]string, error) {
 		return c.xpath(tmpl, ts, vr, ca, v)
