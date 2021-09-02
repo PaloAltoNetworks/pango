14998e6a46b490dd7add1131f4b9a9d6ff7032b0
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-01 20:43:37 -0700

    add ipv6 address config load

--- netw/interface/ipv6/address/pano.go
+++ netw/interface/ipv6/address/pano.go
@@ -70,6 +70,13 @@ func (c *Panorama) Delete(tmpl, ts, iType, iName, subName string, e ...interface
 	return c.ns.Delete(c.pather(tmpl, ts, iType, iName, subName), names, nErr)
 }
 
+// FromPanosConfig retrieves all objects stored in the retrieved config.
+func (c *Panorama) FromPanosConfig(tmpl, ts, iType, iName, subName string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.FromPanosConfig(c.pather(tmpl, ts, iType, iName, subName), ans)
+	return all(ans, err)
+}
+
 func (c *Panorama) pather(tmpl, ts, iType, iName, subName string) namespace.Pather {
 	return func(v []string) ([]string, error) {
 		return c.xpath(tmpl, ts, iType, iName, subName, v)
