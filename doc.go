4bd184fdbb5d56672cbbe2aea6a05c2b5b02d34d
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-01 20:40:24 -0700

    docs: updating docs to mention config load and usage

--- doc.go
+++ doc.go
@@ -60,6 +60,19 @@ For any version safe object, attempting to configure a parameter that your
 PAN-OS doesn't support will be safely ignored in the resultant XML sent to the
 firewall / Panorama.
 
+
+Loading PAN-OS Config
+
+A PAN-OS configuration can be loaded from a PAN-OS device using `RetrievePanosConfig()`
+to pull it from a live device or `LoadPanosConfig()` if already in local memory.  Once
+it's been loaded, the `FromPanosConfig()` namespace functions can be used to retrieve
+normalized objects from the loaded config.
+
+You can use this file load and config retrieval to do offline inspection of the
+config as well, just make sure to set `pango.Client.Version` to the appropriate PAN-OS
+version so the version normalization can take place.
+
+
 Using Edit Functions
 
 The PAN-OS XML API Edit command can be used to both create as well as update
