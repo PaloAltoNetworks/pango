df7de686434d8653a804a89a28b7350a5aa98033
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-09-01 20:56:31 -0700

    add client.Network.BgpAggAdvertiseFilter support

--- netw/routing/protocol/bgp/aggregate/filter/advertise/fw.go
+++ netw/routing/protocol/bgp/aggregate/filter/advertise/fw.go
@@ -70,6 +70,13 @@ func (c *Firewall) Delete(vr, ag string, e ...interface{}) error {
 	return c.ns.Delete(c.pather(vr, ag), names, nErr)
 }
 
+// FromPanosConfig retrieves all objects stored in the retrieved config.
+func (c *Firewall) FromPanosConfig(vr, ag string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.FromPanosConfig(c.pather(vr, ag), ans)
+	return all(ans, err)
+}
+
 func (c *Firewall) pather(vr, ag string) namespace.Pather {
 	return func(v []string) ([]string, error) {
 		return c.xpath(vr, ag, v)
--- netw/routing/protocol/bgp/aggregate/filter/advertise/pano.go
+++ netw/routing/protocol/bgp/aggregate/filter/advertise/pano.go
@@ -70,6 +70,13 @@ func (c *Panorama) Delete(tmpl, ts, vr, ag string, e ...interface{}) error {
 	return c.ns.Delete(c.pather(tmpl, ts, vr, ag), names, nErr)
 }
 
+// FromPanosConfig retrieves all objects stored in the retrieved config.
+func (c *Panorama) FromPanosConfig(tmpl, ts, vr, ag string) ([]Entry, error) {
+	ans := c.container()
+	err := c.ns.FromPanosConfig(c.pather(tmpl, ts, vr, ag), ans)
+	return all(ans, err)
+}
+
 func (c *Panorama) pather(tmpl, ts, vr, ag string) namespace.Pather {
 	return func(v []string) ([]string, error) {
 		return c.xpath(tmpl, ts, vr, ag, v)
