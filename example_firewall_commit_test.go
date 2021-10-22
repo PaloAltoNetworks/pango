76161ca12fceb1ed647825cae839cc591685f6f4
Piyush Pangtey <gokuvsvegita@gmail.com>
2021-10-22 07:44:40 +0530

    fixes #82 - add extras parameter to client.WaitForJob

--- example_firewall_commit_test.go
+++ example_firewall_commit_test.go
@@ -70,7 +70,7 @@ func Example_firewallCommit() {
 		log.Fatalf("Error in commit: %s", err)
 	} else if jobId == 0 {
 		log.Printf("No commit needed")
-	} else if err = fw.WaitForJob(jobId, sd, nil); err != nil {
+	} else if err = fw.WaitForJob(jobId, sd, nil, nil); err != nil {
 		log.Printf("Error in commit: %s", err)
 	} else {
 		log.Printf("Committed config successfully")
