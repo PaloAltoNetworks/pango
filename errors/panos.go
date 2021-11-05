b2fca3f75f6d279eaaa9f94ba3765c737e3c036e
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-11-05 10:05:45 -0700

    feat: Add new error parsing to the errors package

--- errors/panos.go
+++ errors/panos.go
@@ -1,5 +1,13 @@
 package errors
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strings"
+
+	"github.com/PaloAltoNetworks/pango/util"
+)
+
 // Panos is an error returned from PAN-OS.
 //
 // The error contains both the error message and the code returned from PAN-OS.
@@ -23,5 +31,100 @@ func ObjectNotFound() Panos {
 	return Panos{
 		Msg:  "Object not found",
 		Code: 7,
+	}
+}
+
+// Parse attempts to parse an error from the given XML response.
+func Parse(body []byte) error {
+	var e errorCheck
+
+	_ = xml.Unmarshal(body, &e)
+	if e.Failed() {
+		return Panos{
+			Msg:  e.Message(),
+			Code: e.Code,
+		}
+	}
+
+	return nil
+}
+
+type errorCheck struct {
+	XMLName xml.Name       `xml:"response"`
+	Status  string         `xml:"status,attr"`
+	Code    int            `xml:"code,attr"`
+	Msg     *errorCheckMsg `xml:"msg"`
+}
+
+type errorCheckMsg struct {
+	Line    []util.CdataText `xml:"line"`
+	Message string           `xml:",chardata"`
+}
+
+func (e *errorCheck) Failed() bool {
+	if e.Status == "failed" || e.Status == "error" {
+		return true
+	} else if e.Code == 0 || e.Code == 19 || e.Code == 20 {
+		return false
+	}
+
+	return true
+}
+
+func (e *errorCheck) Message() string {
+	if e.Msg == nil {
+		return e.CodeError()
+	}
+
+	if len(e.Msg.Line) > 0 {
+		var b strings.Builder
+		for i := range e.Msg.Line {
+			if i != 0 {
+				b.WriteString(" | ")
+			}
+			b.WriteString(strings.TrimSpace(e.Msg.Line[i].Text))
+		}
+		return b.String()
+	}
+
+	if e.Msg.Message != "" {
+		return e.Msg.Message
+	}
+
+	return ""
+}
+
+func (e *errorCheck) CodeError() string {
+	switch e.Code {
+	case 1:
+		return "Unknown command"
+	case 2, 3, 4, 5, 11:
+		return fmt.Sprintf("Internal error (%d) encountered", e.Code)
+	case 6:
+		return "Bad Xpath"
+	case 7:
+		return "Object not found"
+	case 8:
+		return "Object not unique"
+	case 10:
+		return "Reference count not zero"
+	case 12:
+		return "Invalid object"
+	case 14:
+		return "Operation not possible"
+	case 15:
+		return "Operation denied"
+	case 16:
+		return "Unauthorized"
+	case 17:
+		return "Invalid command"
+	case 18:
+		return "Malformed command"
+	case 0, 19, 20:
+		return ""
+	case 22:
+		return "Session timed out"
+	default:
+		return fmt.Sprintf("(%d) Unknown failure code, operation failed", e.Code)
 	}
 }
---
+++ errors/panos_test.go
@@ -0,0 +1,56 @@
+package errors
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestGetSingularMissingObjectIsError(t *testing.T) {
+	data := `<response status="success" code="7"><result/></response>`
+
+	err := Parse([]byte(data))
+	if err == nil {
+		t.Errorf("Error is nil")
+	} else {
+		e2, ok := err.(Panos)
+		if !ok {
+			t.Errorf("Not a panos error")
+		} else if !e2.ObjectNotFound() {
+			t.Errorf("Not an object not found error")
+		}
+	}
+}
+
+func TestShowSingularMissingObjectIsError(t *testing.T) {
+	data := `<response status="error"><msg><line>No such node</line></msg></response>`
+
+	err := Parse([]byte(data))
+	if err == nil {
+		t.Errorf("Error is nil")
+	} else {
+		e2, ok := err.(Panos)
+		if !ok {
+			t.Errorf("Not a panos error")
+		} else if e2.Msg != "No such node" {
+			t.Errorf("Incorrect msg: %s", e2.Msg)
+		}
+	}
+}
+
+func TestMultilineErrorMessage(t *testing.T) {
+	expected := "HTTP method must be GET"
+	data := fmt.Sprintf(`<response status="error" code="12"><msg><line><![CDATA[ tf123456 -> server -> first server  is invalid. %s, Username/Password must not be empty when Tag Distribution is chosen]]></line><line><![CDATA[ tf123456 -> server is invalid]]></line></msg></response>`, expected)
+
+	err := Parse([]byte(data))
+	if err == nil {
+		t.Errorf("Error is nil")
+	} else {
+		e2, ok := err.(Panos)
+		if !ok {
+			t.Errorf("Not a panos error")
+		} else if !strings.Contains(e2.Msg, expected) {
+			t.Errorf("Does not contain the expected substring: %s", e2.Msg)
+		}
+	}
+}
