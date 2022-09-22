cc42e247f96ebdae63f64412df81e00613e0b36c
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-09-22 12:19:30 -0700

    chore: make new namespaces available

--- dev/fw.go
+++ dev/fw.go
@@ -10,11 +10,18 @@ import (
 	hapath "github.com/PaloAltoNetworks/pango/dev/ha/monitor/path"
 	"github.com/PaloAltoNetworks/pango/dev/localuserdb/group"
 	"github.com/PaloAltoNetworks/pango/dev/localuserdb/user"
+	"github.com/PaloAltoNetworks/pango/dev/profile/authentication"
 	"github.com/PaloAltoNetworks/pango/dev/profile/certificate"
 	"github.com/PaloAltoNetworks/pango/dev/profile/email"
 	"github.com/PaloAltoNetworks/pango/dev/profile/http"
+	"github.com/PaloAltoNetworks/pango/dev/profile/kerberos"
+	"github.com/PaloAltoNetworks/pango/dev/profile/ldap"
+	"github.com/PaloAltoNetworks/pango/dev/profile/radius"
+	"github.com/PaloAltoNetworks/pango/dev/profile/saml"
 	"github.com/PaloAltoNetworks/pango/dev/profile/snmp"
+	"github.com/PaloAltoNetworks/pango/dev/profile/ssltls"
 	"github.com/PaloAltoNetworks/pango/dev/profile/syslog"
+	"github.com/PaloAltoNetworks/pango/dev/profile/tacplus"
 	"github.com/PaloAltoNetworks/pango/dev/ssldecrypt"
 	"github.com/PaloAltoNetworks/pango/dev/telemetry"
 	"github.com/PaloAltoNetworks/pango/dev/vminfosource"
@@ -22,39 +29,53 @@ import (
 
 // Firewall is the client.Device namespace.
 type Firewall struct {
-	Certificate         *cert.Firewall
-	CertificateProfile  *certificate.Firewall
-	EmailServerProfile  *email.Firewall
-	GeneralSettings     *general.Firewall
-	HaConfig            *ha.Firewall
-	HaLinkMonitorGroup  *halink.Firewall
-	HaPathMonitorGroup  *hapath.Firewall
-	HttpServerProfile   *http.Firewall
-	LocalUserDbGroup    *group.Firewall
-	LocalUserDbUser     *user.Firewall
-	SslDecrypt          *ssldecrypt.Firewall
-	SnmpServerProfile   *snmp.Firewall
-	SyslogServerProfile *syslog.Firewall
-	Telemetry           *telemetry.Firewall
-	VmInfoSource        *vminfosource.Firewall
+	AuthenticationProfile *authentication.Firewall
+	Certificate           *cert.Firewall
+	CertificateProfile    *certificate.Firewall
+	EmailServerProfile    *email.Firewall
+	GeneralSettings       *general.Firewall
+	HaConfig              *ha.Firewall
+	HaLinkMonitorGroup    *halink.Firewall
+	HaPathMonitorGroup    *hapath.Firewall
+	HttpServerProfile     *http.Firewall
+	KerberosProfile       *kerberos.Firewall
+	LdapProfile           *ldap.Firewall
+	LocalUserDbGroup      *group.Firewall
+	LocalUserDbUser       *user.Firewall
+	RadiusProfile         *radius.Firewall
+	SamlProfile           *saml.Firewall
+	SslDecrypt            *ssldecrypt.Firewall
+	SslTlsServiceProfile  *ssltls.Firewall
+	SnmpServerProfile     *snmp.Firewall
+	SyslogServerProfile   *syslog.Firewall
+	TacacsPlusProfile     *tacplus.Firewall
+	Telemetry             *telemetry.Firewall
+	VmInfoSource          *vminfosource.Firewall
 }
 
 func FirewallNamespace(x util.XapiClient) *Firewall {
 	return &Firewall{
-		Certificate:         cert.FirewallNamespace(x),
-		CertificateProfile:  certificate.FirewallNamespace(x),
-		EmailServerProfile:  email.FirewallNamespace(x),
-		GeneralSettings:     general.FirewallNamespace(x),
-		HaConfig:            ha.FirewallNamespace(x),
-		HaLinkMonitorGroup:  halink.FirewallNamespace(x),
-		HaPathMonitorGroup:  hapath.FirewallNamespace(x),
-		HttpServerProfile:   http.FirewallNamespace(x),
-		LocalUserDbGroup:    group.FirewallNamespace(x),
-		LocalUserDbUser:     user.FirewallNamespace(x),
-		SslDecrypt:          ssldecrypt.FirewallNamespace(x),
-		SnmpServerProfile:   snmp.FirewallNamespace(x),
-		SyslogServerProfile: syslog.FirewallNamespace(x),
-		Telemetry:           telemetry.FirewallNamespace(x),
-		VmInfoSource:        vminfosource.FirewallNamespace(x),
+		AuthenticationProfile: authentication.FirewallNamespace(x),
+		Certificate:           cert.FirewallNamespace(x),
+		CertificateProfile:    certificate.FirewallNamespace(x),
+		EmailServerProfile:    email.FirewallNamespace(x),
+		GeneralSettings:       general.FirewallNamespace(x),
+		HaConfig:              ha.FirewallNamespace(x),
+		HaLinkMonitorGroup:    halink.FirewallNamespace(x),
+		HaPathMonitorGroup:    hapath.FirewallNamespace(x),
+		HttpServerProfile:     http.FirewallNamespace(x),
+		KerberosProfile:       kerberos.FirewallNamespace(x),
+		LdapProfile:           ldap.FirewallNamespace(x),
+		LocalUserDbGroup:      group.FirewallNamespace(x),
+		LocalUserDbUser:       user.FirewallNamespace(x),
+		RadiusProfile:         radius.FirewallNamespace(x),
+		SamlProfile:           saml.FirewallNamespace(x),
+		SslTlsServiceProfile:  ssltls.FirewallNamespace(x),
+		SslDecrypt:            ssldecrypt.FirewallNamespace(x),
+		SnmpServerProfile:     snmp.FirewallNamespace(x),
+		SyslogServerProfile:   syslog.FirewallNamespace(x),
+		TacacsPlusProfile:     tacplus.FirewallNamespace(x),
+		Telemetry:             telemetry.FirewallNamespace(x),
+		VmInfoSource:          vminfosource.FirewallNamespace(x),
 	}
 }
--- dev/pano.go
+++ dev/pano.go
@@ -6,41 +6,62 @@ import (
 	cert "github.com/PaloAltoNetworks/pango/dev/certificate"
 	"github.com/PaloAltoNetworks/pango/dev/localuserdb/group"
 	"github.com/PaloAltoNetworks/pango/dev/localuserdb/user"
+	"github.com/PaloAltoNetworks/pango/dev/profile/authentication"
 	"github.com/PaloAltoNetworks/pango/dev/profile/certificate"
 	"github.com/PaloAltoNetworks/pango/dev/profile/email"
 	"github.com/PaloAltoNetworks/pango/dev/profile/http"
+	"github.com/PaloAltoNetworks/pango/dev/profile/kerberos"
+	"github.com/PaloAltoNetworks/pango/dev/profile/ldap"
+	"github.com/PaloAltoNetworks/pango/dev/profile/radius"
+	"github.com/PaloAltoNetworks/pango/dev/profile/saml"
 	"github.com/PaloAltoNetworks/pango/dev/profile/snmp"
+	"github.com/PaloAltoNetworks/pango/dev/profile/ssltls"
 	"github.com/PaloAltoNetworks/pango/dev/profile/syslog"
+	"github.com/PaloAltoNetworks/pango/dev/profile/tacplus"
 	"github.com/PaloAltoNetworks/pango/dev/ssldecrypt"
 	"github.com/PaloAltoNetworks/pango/dev/vminfosource"
 )
 
 // Panorama is the client.Device namespace.
 type Panorama struct {
-	Certificate         *cert.Panorama
-	CertificateProfile  *certificate.Panorama
-	EmailServerProfile  *email.Panorama
-	HttpServerProfile   *http.Panorama
-	LocalUserDbGroup    *group.Panorama
-	LocalUserDbUser     *user.Panorama
-	SslDecrypt          *ssldecrypt.Panorama
-	SnmpServerProfile   *snmp.Panorama
-	SyslogServerProfile *syslog.Panorama
-	VmInfoSource        *vminfosource.Panorama
+	AuthenticationProfile *authentication.Panorama
+	Certificate           *cert.Panorama
+	CertificateProfile    *certificate.Panorama
+	EmailServerProfile    *email.Panorama
+	HttpServerProfile     *http.Panorama
+	KerberosProfile       *kerberos.Panorama
+	LdapProfile           *ldap.Panorama
+	LocalUserDbGroup      *group.Panorama
+	LocalUserDbUser       *user.Panorama
+	RadiusProfile         *radius.Panorama
+	SamlProfile           *saml.Panorama
+	SslTlsServiceProfile  *ssltls.Panorama
+	SslDecrypt            *ssldecrypt.Panorama
+	SnmpServerProfile     *snmp.Panorama
+	SyslogServerProfile   *syslog.Panorama
+	TacacsPlusProfile     *tacplus.Panorama
+	VmInfoSource          *vminfosource.Panorama
 }
 
 // Initialize is invoked on client.Initialize().
 func PanoramaNamespace(x util.XapiClient) *Panorama {
 	return &Panorama{
-		Certificate:         cert.PanoramaNamespace(x),
-		CertificateProfile:  certificate.PanoramaNamespace(x),
-		EmailServerProfile:  email.PanoramaNamespace(x),
-		HttpServerProfile:   http.PanoramaNamespace(x),
-		LocalUserDbGroup:    group.PanoramaNamespace(x),
-		LocalUserDbUser:     user.PanoramaNamespace(x),
-		SslDecrypt:          ssldecrypt.PanoramaNamespace(x),
-		SnmpServerProfile:   snmp.PanoramaNamespace(x),
-		SyslogServerProfile: syslog.PanoramaNamespace(x),
-		VmInfoSource:        vminfosource.PanoramaNamespace(x),
+		AuthenticationProfile: authentication.PanoramaNamespace(x),
+		Certificate:           cert.PanoramaNamespace(x),
+		CertificateProfile:    certificate.PanoramaNamespace(x),
+		EmailServerProfile:    email.PanoramaNamespace(x),
+		HttpServerProfile:     http.PanoramaNamespace(x),
+		KerberosProfile:       kerberos.PanoramaNamespace(x),
+		LdapProfile:           ldap.PanoramaNamespace(x),
+		LocalUserDbGroup:      group.PanoramaNamespace(x),
+		LocalUserDbUser:       user.PanoramaNamespace(x),
+		RadiusProfile:         radius.PanoramaNamespace(x),
+		SamlProfile:           saml.PanoramaNamespace(x),
+		SslTlsServiceProfile:  ssltls.PanoramaNamespace(x),
+		SslDecrypt:            ssldecrypt.PanoramaNamespace(x),
+		SnmpServerProfile:     snmp.PanoramaNamespace(x),
+		SyslogServerProfile:   syslog.PanoramaNamespace(x),
+		TacacsPlusProfile:     tacplus.PanoramaNamespace(x),
+		VmInfoSource:          vminfosource.PanoramaNamespace(x),
 	}
 }
