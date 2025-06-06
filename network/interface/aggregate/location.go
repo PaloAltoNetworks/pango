46055d7abd474cc7a38c3da47cafea52ef0d7647
mrhinton101 <32552582+mrhinton101@users.noreply.github.com>
2025-06-06 13:32:32 -0400

    Update location.go
    remove unrelated linting

--- network/interface/aggregate/location.go
+++ network/interface/aggregate/location.go
@@ -39,7 +39,8 @@ type NgfwLocation struct {
 }
 
 func NewSharedLocation() *Location {
-	return &Location{Shared: &SharedLocation{}}
+	return &Location{Shared: &SharedLocation{},
+	}
 }
 func NewTemplateLocation() *Location {
 	return &Location{Template: &TemplateLocation{
